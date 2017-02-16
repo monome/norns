97d61ffb7e13a8e51a9111925373ff10cf09ff9b
emb <emb@catfact.net>
2017-02-15 22:44:46 -0500

    launcher: use plain ol pipes for redirection, intsead of unix sockets

--- norns.c
+++ norns.c
@@ -5,26 +5,25 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define MATRON_SOCK_NAME "./tmp/matron_sock"
-#define CRONE_SOCK_NAME "./tmp/crone_sock"
-#define CLIENT_BUFSIZE 1024
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+
+int matron_pipe_in[2];
+int matron_pipe_out[2];
+
+int crone_pipe_out[2];
 
 // child process id's
 pid_t matron_pid;
 pid_t crone_pid;
 
 // client thread id's
-pthread_t matron_client_tid, matron_client_rx_tid;
-pthread_t crone_client_tid;
+pthread_t matron_tx_tid, matron_rx_tid;
+pthread_t crone_rx_tid;
 
-// utility to initialize local socket data
-void init_sock(struct sockaddr_un *addr, const char* name) {
-  memset(addr, 0, sizeof(struct sockaddr_un));
-  addr->sun_family = AF_UNIX;
-  sprintf(addr->sun_path, name);
-}
+#define BUFFER_SIZE 1024
 
-// utilitiy to launch a joinable thread with no data
+// utility to launch a joinable thread
 void launch_thread(pthread_t *tid, void *(*start_routine) (void *), void* data) {
   pthread_attr_t attr;
   int s;
@@ -35,65 +34,50 @@ void launch_thread(pthread_t *tid, void *(*start_routine) (void *), void* data) 
   pthread_attr_destroy(&attr);
 }
 
-// on connection to the matron client,
-// launch the executable and redirect stdio to the connected socket
-void launch_matron(int sock) {
-  char *argv[] = { "matron", "57120", "8888", NULL};
-  char *env[] = { NULL };
-
-  // redirect stdout, stderr
-  dup2( sock, STDOUT_FILENO );
-  dup2( sock, STDERR_FILENO );
-  // redirect stdin to the same socket... seems to work...
-  dup2( sock, STDIN_FILENO );
-  // no longer need the original handle
-  close(sock);
-	
-  execv("matron/matron", argv);
-  perror("execv");
-}
-
-// start a local socket serer wrapping the matron executable
-void run_matron(void) {
-  struct sockaddr_un addr;
-  int sock, connect;
-  int res;
-  socklen_t addr_len;
-
-  sock = socket(PF_UNIX, SOCK_STREAM, 0);
-  if(sock <0) { printf("socket() failed \n"); return; }
-  
-  unlink(MATRON_SOCK_NAME);
-  init_sock(&addr, MATRON_SOCK_NAME);
-  
-  res = bind(sock,
-			 (struct sockaddr *) &addr, 
-			 sizeof(struct sockaddr_un));
-  if(res != 0) { printf("bind() failed \n"); return; }
-
-  res = listen(sock, 5);
-  if(res!= 0) { printf("listen() failed\n"); return; }
-
-  // wait for the client to connect (we only care about the first connection)
-  connect = accept(sock, (struct sockaddr *) &addr, &addr_len);
-  if(connect > 0) { 
-	launch_matron(connect);
-  } else {
-	printf("error connecting to matron server \n");
+// run the matron executable and redirect its i/o
+int run_matron(void) {
+  if(matron_pid != 0) {
+	printf("error: calling run_matron() from parent process \n");
+	return -1;
   }
+      // redirect stdin
+    if (dup2(matron_pipe_in[PIPE_READ], STDIN_FILENO) == -1) {
+      perror("redirecting stdin");
+      return -1;
+    }
+
+    // redirect stdout
+    if (dup2(matron_pipe_out[PIPE_WRITE], STDOUT_FILENO) == -1) {
+      perror("redirecting stdout");
+      return -1;
+    }
+
+    // redirect stderr
+    if (dup2(matron_pipe_out[PIPE_WRITE], STDERR_FILENO) == -1) {
+      perror("redirecting stderr");
+      return -1;
+    }
+
+    close(matron_pipe_in[PIPE_READ]);
+    close(matron_pipe_in[PIPE_WRITE]);
+    close(matron_pipe_out[PIPE_READ]);
+    close(matron_pipe_out[PIPE_WRITE]); 
+
+	char *argv[] = { "matron", "57120", "8888", NULL};
+	char *env[] = { NULL };
+	
+	execv("matron/matron", argv);
+	perror("execv");
   
 }
 
-// run the client that will glue stdin/stdout to matron by local socket
-
-// rx thread
-void* matron_client_rx(void* psock) {
-  char rxbuf[CLIENT_BUFSIZE];
+// matron rx thread
+void* matron_rx(void* psock) {
+  char rxbuf[BUFFER_SIZE];
   int nb;
-  int sock = *((int*)psock);
   while(1) { 
 	// print server response
-	nb = read(sock, rxbuf, CLIENT_BUFSIZE-1);
+	nb = read(matron_pipe_out[PIPE_READ], rxbuf, BUFFER_SIZE-1);
 	if(nb > 0) {
 	  rxbuf[nb] = '\0';
 	  printf("%s", rxbuf);
@@ -101,36 +85,16 @@ void* matron_client_rx(void* psock) {
   }
 }
 
-// connect / tx thread
-void* matron_client(void* x) {
-  struct sockaddr_un addr;
-  int sock;
-  char txbuf[CLIENT_BUFSIZE];
+// matron tx thread
+void* matron_tx(void* x) {
+  char txbuf[BUFFER_SIZE];
   int res;
   int nb;
   int ch;
   int newline = 0;
 
-  sock = socket(PF_UNIX, SOCK_STREAM, 0);
-  if(sock < 0) {
-	printf("run_matron_client(): socket() failed\n");
-	return NULL;
-  }
-  
-  init_sock(&addr, MATRON_SOCK_NAME);
-  // wait a bit for the server
+  // wait a bit for the child executable
   usleep(100000);
-  
-  res = connect(sock,
-				(struct sockaddr*)&addr,
-				sizeof(struct sockaddr_un));
-  if(res != 0) {
-	printf("connect() failed \n");
-	return NULL;
-  }
-
-  // launch the rx thread with the connected socket
-  launch_thread(&matron_client_rx_tid, &matron_client_rx, &sock);  
   
   while(1) {
 	// FIXME: weird issues with getline() and threads/children (?)
@@ -141,7 +105,7 @@ void* matron_client(void* x) {
 	//	printf("waiting for input\n");
 	// read from stdin
 	while(newline != 1) {
-	  if(nb < (CLIENT_BUFSIZE-1)) {
+	  if(nb < (BUFFER_SIZE-1)) {
 		ch = getchar();
 		txbuf[nb++] = (char)ch;
 		if(ch == 10) { newline = 1; }
@@ -152,7 +116,7 @@ void* matron_client(void* x) {
 	txbuf[nb] = '\0';	
 	// send to server
 	//	printf("sending %dB to server: \n%s\n", nb, txbuf);
-	write(sock, txbuf, nb);
+	write(matron_pipe_in[PIPE_WRITE], txbuf, nb);
 	//	printf("... sent\n");
   }
 }
@@ -163,13 +127,36 @@ void run_crone(void) {
 }
 
 
-void* crone_client(void* x) {
+void* crone_rx(void* x) {
   // TODO
   while(1) { ;; }
 }
 
+
+void cleanup_pipe(int pipe[2]) {
+  close(pipe[PIPE_READ]);
+  close(pipe[PIPE_WRITE]);
+}
+
 int main(int argc, char** argv) {
 
+  // create pipes for i/o redirection
+  if(pipe(matron_pipe_in) < 0) {
+	perror("allocating pipe for matron input redirect");
+	return -1;
+  }
+    if(pipe(matron_pipe_out) < 0) {
+	perror("allocating pipe for matron output redirect");
+	cleanup_pipe(matron_pipe_in);
+	return -1;
+  }
+  if(pipe(crone_pipe_out) < 0) {
+	perror("allocating pipe for crone output redirect");
+	cleanup_pipe(matron_pipe_in);
+	cleanup_pipe(matron_pipe_out);
+	return -1;
+  }
+  
   // fork a child process to run matron
   matron_pid = fork();
   if(matron_pid < 0) {
@@ -189,18 +176,26 @@ int main(int argc, char** argv) {
 	printf("fork() returned an error\n");
 	return 1;
   }
+  
   if(crone_pid == 0) {
 	run_crone();
 	goto child_exit; // shouldn't get here
   }
 
+  // close unused pipe endpoints
+  close(matron_pipe_in[PIPE_READ]);
+  close(matron_pipe_out[PIPE_WRITE]);
+  close(crone_pipe_out[PIPE_WRITE]);
+  
   // create threads to handle the children's i/o
-  launch_thread(&matron_client_tid, &matron_client, NULL);
-  launch_thread(&crone_client_tid, &crone_client, NULL);
+  launch_thread(&matron_tx_tid, &matron_tx, NULL);
+  launch_thread(&matron_rx_tid, &matron_rx, NULL);
+  launch_thread(&crone_rx_tid, &crone_rx, NULL);
   
   // wait for threads to exit
-  pthread_join(matron_client_tid, NULL);
-  pthread_join(crone_client_tid, NULL);
+  pthread_join(matron_tx_tid, NULL);
+  pthread_join(matron_rx_tid, NULL);
+  pthread_join(crone_rx_tid, NULL);
  
   // wait for child processes to exit
   wait(0);
