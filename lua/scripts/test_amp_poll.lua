20dfaf217476344ec0937cc993e6533cdbadf89b
emb <emb@catfact.net>
2017-10-25 10:52:17 -0400

    woops, missing test_amp_poll script. also add new API example

---
+++ lua/scripts/test_amp_poll.lua
@@ -0,0 +1,33 @@
+require 'math'
+
+print("test_amp_poll.lua")
+
+local function printAsciiMeter(amp, n, floor)
+   if n == nil then n = 64 end
+   if floor == nil then floor = -72 end
+   local db = 20.0 * math.log10(amp)
+   local norm = 1.0 - (db / floor)
+   local x = norm * n
+   local str = ""
+   for i=0,x do
+      str = str.."#"
+   end
+   print(str)
+end
+
+poll = function(poll, value)
+   if poll.name == "amp_in_l" then
+      printAsciiMeter(value)
+   end
+end
+
+report.polls = function()
+   if norns.polls['amp_in_l'] ~= nil then
+	 -- FIXME: not a great interface.
+	 -- poll objects should have "set_time". "start", "stop" methods...
+	 norns.set_poll_time('amp_in_l', 0.05)
+	 norns.start_poll('amp_in_l');
+   end
+end
+
+report_polls()
