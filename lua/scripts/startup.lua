2deb0e191fdd5e32575186c7881a7cdc4990d6e0
emb <emb@catfact.net>
2017-10-29 04:10:32 -0400

    fix polls, test_amp_poll for new api

--- lua/scripts/startup.lua
+++ lua/scripts/startup.lua
@@ -3,10 +3,9 @@ print("startup.lua")
 
 --- here are some tests
 
-require('test_engine')  -- OK
+--require('test_engine')  -- OK
 -- require('test_grid') -- OK
 -- require('test_input_devices') -- OK
 
--- require('test_amp_poll') -- ?
+require('test_amp_poll') -- NO
 -- require('test_time_query') -- ?
--- require('test_grid_timer') -- NO
--- lua/sys/poll.lua
+++ lua/sys/poll.lua
@@ -6,30 +6,43 @@
 Poll = {}
 Poll.__index = Poll
 
+
+--- poll objects (index by name)
+Poll.polls = {}
+--- poll names (indexed by int) - for reverse lookup
+Poll.pollNames = {}
+
 -- constructor
 function Poll.new(props)
-   local self = setmetatable({}, Poll)
-   if props then
-      if props.id && props.name then
-	 self.props = props
-	 return self
+   local p = {}
+      if props then
+      if props.id and props.name then
+	 p.props = props
       end
+   else
+      props = {}
+      print("warning: Poll constructor requires at least name and id properties")
    end
-   print("warning: Poll constructor requires at least name and id properties")
-   return nil
+   setmetatable(p, Poll)
+   return p
 end
+
+--- static report callback;
+-- user script should redefine if needed
+-- @param polls : table of polls
+Poll.report = function(polls) end
 
 --- Instance Methods
 -- @section instance
 
 --- start a poll
 function Poll:start()
-   start_poll(self.id)
+   start_poll(self.props.id)
 end
 
 --- stop a poll
 function Poll:stop()
-   stop_poll(self.id)
+   stop_poll(self.props.id)
 end
 
 
@@ -37,7 +50,7 @@ end
 --- FIXME: need explicit tags or something for ldoc
 function Poll:__newindex(idx, val)
    if idx == 'time' then
-      self.props[time] = val
+      self.props.time = val
       set_poll_time(self.props.id, val)
    elseif idx == 'callback' then
       self.props.callback = val
@@ -50,8 +63,9 @@ end
 function Poll:__index(idx)
    if idx == 'id' then return self.props.id
    elseif idx == 'name' then return self.props.name
+   elseif idx == 'callback' then return self.props.callback
    elseif idx == 'start' then return Poll.start
-   elseif idx == 'stop' then return Poll.start
+   elseif idx == 'stop' then return Poll.stop
    else
       return rawget(self, idx)
    end      
@@ -70,11 +84,12 @@ Poll.register = function(data, count)
    local props
    for i=1,count do
       props = {
-	 id = data[i][1]
+	 id = data[i][1],
 	 name = data[i][2]
       }
+      print(props.id, props.name)
       Poll.pollNames[props.id] = props.name
-      Poll.pollNames[props.name] = Poll.new(props)
+      Poll.polls[props.name] = Poll.new(props)      
    end
 end
 
@@ -83,83 +98,43 @@ end
 -- @param callback function to call with value on each poll
 Poll.named = function(name, callback)
    local p = Poll.polls[name]
-   p.props.callback = callback   
+   if(p) then
+      p.props.callback = callback
+   end
 end
 
 --- Globals
 -- @section globals
 
---- poll objects (index by name)
-norns.polls = {}
-
---- poll names (indexed by int) - for reverse lookup
-norns.pollNames = {}
-
 --- poll report callback; called from C
 norns.report.polls = function(polls, count)
    Poll.register(polls, count)
+   Poll.report(Poll.polls)
 end
-
-
---[[
--norns.report.polls = function(polls, count)
--   print("norns.report.polls ; count: " .. count)
----[[
-
--   norns.pollNames = {}
--   local t, name, idx
--   for i=1,count do
--      print("poll " .. polls[i][1] .. " : " .. polls[i][2] .. " type: " .. polls[i][3])
--      idx = polls[i][1]
--      name = polls[i][2]
--
---- FIXME
--      
----      norns.polls[name] = Poll(polls[i][1], polls[i][2], polls[i][3])
----      norns.polls[name] = {}
----      norns.polls[name].idx = polls[i][1]
----      norns.polls[name].name = polls[i][2]
----      norns.polls[name].type= polls[i][3]
--      norns.pollNames[idx] = name;
--   end
--   -- call the script-defined report callback, if it exists
--   if report.polls ~= nil then report.polls(polls, count) end
-
--end
---]]
-
 
 --- main callback; called from C
 -- @param id - identfier (integer)
 -- @param value: value (float OR sequence of bytes)
 norns.poll = function(id, value)
-   local p = Poll.polls[Poll.pollNames[id]]
+   local name = Poll.pollNames[id]
+   local p = Poll.polls[name]
+   -- print(id, name, p)
    if p then
       if p.props then
-	 if type(p.props.callback) == "function" then
-	    p.props.callback(value)
+	 if p.props.callback then 
+	    if type(p.props.callback) == "function" then
+	       p.props.callback(value)
+	    end
+	 else
+	    -- print("no callback") -- ok
 	 end
+      else
+	 print("no props") assert(false)
       end
+   else
+      print ("no poll") assert(false)
    end
 end
 
---[[
-norns.poll = function(idx, arg)
-   --- FIXME: testing
-   local name = nil
-   print("norns.poll: "..idx .. " "..arg)
-   if norns.pollNames[idx] ~= nil then
-      name = norns.pollNames[idx]
-      poll = norns.polls[idx]
-      
-      -- print("norns.poll: "..idx..": " ..name..": "..arg)
-      -- call script-defined poll callback
-      --      if poll ~= nil then poll(norns.polls[name], arg) end
-      
-   else
-      print("norns.poll: unknown index: " .. idx .. " "..arg)
-   end
-end
---]]
 
 return Poll
