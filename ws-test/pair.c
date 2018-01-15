9f2be6800e834b92e58f6404b1391ed693380e18
Brian Crabtree <tehn@monome.org>
2018-01-14 21:23:59 -0500

    simplify ws-test, still weird

--- ws-test/pair.c
+++ ws-test/pair.c
@@ -1,80 +1,47 @@
 #include <assert.h>
-//#include <libc.h>
 #include <string.h>
 #include <nanomsg/nn.h>
 #include <unistd.h>
 #include <nanomsg/pair.h>
+#include <nanomsg/nn.h>
 #include <nanomsg/ws.h>
 #include <stdio.h>
 
-#define NODE0 "node0"
-#define NODE1 "node1"
-
-int send_name(int sock, const char *name)
-{
-  printf ("%s: SENDING \"%s\"\n", name, name);
-  int sz_n = strlen (name) + 1; // '\0' too
-  return nn_send (sock, name, sz_n, 0);
-}
-
-int recv_name(int sock, const char *name)
+int echo(int sock)
 {
   char *buf = NULL;
   int result = nn_recv (sock, &buf, NN_MSG, 0);
   if (result > 0)
     {
-      printf ("RECEIVED \"%s\" len: %d\n", buf, strlen(buf)+1);
-      void *msg = nn_allocmsg (128, 0);
-      strcpy(msg,buf);
-      nn_send(sock, msg, strlen(msg)+1, 0);
-      printf ("SEND \"%s\"\n", msg);
+      printf ("RECEIVED \"%s\" len: %d\n", buf, strlen(buf));
+      nn_send(sock, buf, strlen(buf), 0);
+      printf ("SEND \"%s\"\n", buf);
       nn_freemsg (buf);
-      nn_freemsg (msg);
     }
   return result;
 }
 
-int send_recv(int sock, const char *name)
-{
-  //int to = 100;
-  int to = NN_WS_MSG_TYPE_TEXT;
-  //assert (nn_setsockopt (sock, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof (to)) >= 0);
-  assert (nn_setsockopt (sock, NN_WS, NN_WS_MSG_TYPE_TEXT, &to, sizeof (to)) >= 0);
-  while(1)
-    {
-      recv_name(sock, name);
-      sleep(0.1);
-    }
-}
-
-int node0 (const char *url)
-{
-  int sock = nn_socket (AF_SP, NN_PAIR);
-  assert (sock >= 0);
-  assert (nn_bind (sock, url) >= 0);
-  send_recv(sock, NODE0);
-  return nn_shutdown (sock, 0);
-}
-
-int node1 (const char *url)
-{
-  int sock = nn_socket (AF_SP, NN_PAIR);
-  assert (sock >= 0);
-  assert (nn_connect (sock, url) >= 0);
-  send_recv(sock, NODE1);
-  return nn_shutdown (sock, 0);
-}
-
 int main (const int argc, const char **argv)
 {
-  if (strncmp (NODE0, argv[1], strlen (NODE0)) == 0 && argc > 1)
-    return node0 (argv[2]);
-  else if (strncmp (NODE1, argv[1], strlen (NODE1)) == 0 && argc > 1)
-    return node1 (argv[2]);
+  if (argc > 0) {
+    int sock = nn_socket (AF_SP, NN_PAIR);
+    assert (sock >= 0);
+    assert (nn_bind (sock, argv[1]) >= 0);
+
+    int to = 100;
+    assert (nn_setsockopt (sock, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof (to)) >= 0);
+    to = NN_WS_MSG_TYPE_TEXT;
+    assert (nn_setsockopt (sock, NN_WS, NN_WS_MSG_TYPE, &to, sizeof (to)) >= 0);
+
+    while(1)
+        {
+            echo(sock);
+            sleep(0.1);
+        }
+  }
   else
-    {
-      fprintf (stderr, "Usage: pair %s|%s <URL> <ARG> ...\n",
-               NODE0, NODE1);
+  {
+      fprintf (stderr, "Usage: pair <URL>\n");
       return 1;
-    }
+  }
 }
