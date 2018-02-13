f4763d9c09ea7bef569a0c0ce6cc054ed9c61d1c
Brian Crabtree <tehn@monome.org>
2018-02-13 16:29:44 -0500

    osc receive wip

--- matron/src/device/osc.c
+++ matron/src/device/osc.c
@@ -18,15 +18,25 @@
 #include "oracle.h"
 
 static lo_address remote_addr;
-//static lo_server_thread st;
+static lo_server_thread st;
+
+static int osc_receive(const char *path, const char *types,
+			lo_arg **argv, int argc, void *data, void *user_data);
+static void lo_error_handler(int num, const char *m, const char *path);
 
 void osc_init(void) { 
-    // TEST ADDRESS/PORT
-    remote_addr = lo_address_new("192.168.1.6", "9001"); 
+    // arbitrary default destination ip/port
+    remote_addr = lo_address_new("127.0.0.1", "9001"); 
+
+    // receive
+    st = lo_server_thread_new("10111", lo_error_handler); 
+    lo_server_thread_add_method(st, NULL, NULL, osc_receive, NULL); 
+    lo_server_thread_start(st);
 }
 
 void osc_deinit(void) {
     lo_address_free(remote_addr);
+    lo_server_thread_free(st);
 }
 
 void osc_send(const char *path, lo_message msg) {
@@ -37,4 +47,36 @@ void osc_send(const char *path, lo_message msg) {
 void osc_remote_addr(const char *ip, const char *port) {
     free(remote_addr);
     remote_addr = lo_address_new(ip,port);
+}
+
+int osc_receive(const char *path,
+                       const char *types,
+                       lo_arg **argv,
+                       int argc,
+                       void *data,
+                       void *user_data)
+{
+    (void)path;
+    (void)types;
+    (void)argc;
+    (void)argv;
+    (void)data;
+    (void)user_data;
+
+	int i;
+    printf("path: <%s>\n", path);
+    for (i = 0; i < argc; i++) {
+        printf("arg %d '%c' ", i, types[i]);
+        lo_arg_pp((lo_type)types[i], argv[i]);
+        printf("\n");
+    }
+    printf("\n");
+    fflush(stdout);
+
+    return 0;
+}
+
+void lo_error_handler(int num, const char *m, const char *path) {
+    printf("liblo error %d in path %s: %s\n", num, path, m);
+    fflush(stdout);
 }
