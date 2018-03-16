adbff783ef85d59340a34d52d2f699dc94754dab
Brian Crabtree <tehn@monome.org>
2018-03-16 17:58:18 -0400

    add libavahi requirement to setup docs

--- lua/state.lua
+++ lua/state.lua
@@ -1,6 +1,6 @@
 -- state
-sys.file.state.script = 'template.lua'
-sys.file.state.out = '59.0'
-sys.input_left = '30'
-sys.input_right = '30'
-sys.hp = '20.0'
+sys.file.state.script = 'draw.lua'
+sys.file.state.out = '64'
+sys.input_left = '48.0'
+sys.input_right = '48.0'
+sys.hp = '45'
