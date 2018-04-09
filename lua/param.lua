48930aee3a2711ed7bef13b0387373bcb5a9f351
brian crabtree <tehn@monome.org>
2018-04-09 17:02:14 -0400

    paramset write, docs

--- lua/param.lua
+++ lua/param.lua
@@ -1,8 +1,15 @@
+--- Param class
+-- @module paramset
+
 local ControlSpec = require 'controlspec'
 
 local Param = {}
 Param.__index = Param
 
+--- constructor
+-- @param name of param
+-- @param controlspec
+-- @param formatter
 function Param.new(name, controlspec, formatter)
   local p = setmetatable({}, Param)
   if not controlspec then controlspec = ControlSpec.default() end
@@ -19,19 +26,26 @@ function Param.new(name, controlspec, formatter)
   return p
 end
 
-
+--- get
+-- returns mapped value
 function Param:get()
   return self.controlspec:map(self.raw)
 end
 
+--- get_raw
+-- get 0-1
 function Param:get_raw()
   return self.raw
 end
 
+--- set
+-- accepts a mapped value
 function Param:set(value)
   self:set_raw(self.controlspec:unmap(value))
 end
 
+--- set_raw
+-- set 0-1
 function Param:set_raw(value)
   clamped_value = util.clamp(value, 0, 1)
   if self.raw ~= clamped_value then
@@ -40,6 +54,8 @@ function Param:set_raw(value)
   end
 end
 
+--- delta
+-- add delta to current value. checks controlspec for mapped vs not
 function Param:delta(d)
   if self.controlspec.delta_map then
     self:delta_raw(d/self.controlspec.delta_div)
@@ -48,10 +64,13 @@ function Param:delta(d)
   end
 end
 
+--- delta_raw
+-- called by delta() if needed
 function Param:delta_raw(d)
   self:set_raw(self.raw + d)
 end
 
+--- set_default
 function Param:set_default()
   if self.controlspec.default then
     self:set(self.controlspec.default)
@@ -60,11 +79,13 @@ function Param:set_default()
   end
 end
 
+--- bang
 function Param:bang()
   self.action(self:get())
 end
 
-
+--- string
+-- @return formatted string
 function Param:string()
   if self.formatter then
     return self.formatter(self)
--- lua/paramset.lua
+++ lua/paramset.lua
@@ -1,24 +1,46 @@
+--- Paramset class
+-- @module paramset
+
 local Paramset = {}
 
+--- print
+-- @param p table to print
 Paramset.print = function(p)
   for k,v in pairs(p) do
     print(k.." = "..v:get())
   end
 end
 
+--- write to disk
+-- @param p table
+-- @param filename relative to data_dir
 Paramset.write = function(p, filename) 
   local fd=io.open(data_dir .. filename,"w+")
   io.output(fd)
   for k,v in pairs(p) do
     io.write(k..","..v:get().."\n")
+    --print(k..","..v:get())
   end
   io.close(fd)
 end
 
+--- read from disk
+-- @param p table
+-- @param filename relative to data_dir
 Paramset.read = function(p, filename)
-
+  local fd=io.open(data_dir .. filename,"r")
+  if fd then
+    io.close(fd)
+    for line in io.lines(data_dir .. filename) do
+      --print(line)
+      k,v = line:match("([^,]+),([^,]+)")
+      p[k]:set(tonumber(v))
+    end 
+  end 
 end
 
+--- bang all params
+-- @param p table
 Paramset.bang = function(p)
   for k,v in pairs(p) do
     v:bang()
