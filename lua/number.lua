82e45905401148e961025a33eac218c82f0a4442
brian crabtree <tehn@monome.org>
2018-04-10 11:38:56 -0400

    number.lua

---
+++ lua/number.lua
@@ -0,0 +1,47 @@
+--- Number class
+-- @module number
+
+local Number = {}
+Number.__index = Number
+
+function Number.new(name, default, min, max)
+  local o = setmetatable({}, Number)
+  o.name = name
+  o.default = default or 0
+  o.value = o.default
+  o.min = min or -2147483648
+  o.max = max or 2147483647 -- 32 bit signed
+  o.action = function() end
+  return o
+end 
+
+function Number:get()
+  return self.value
+end
+
+function Number:set(v)
+  local c = util.clamp(v,self.min,self.max)
+  if self.value ~= c then
+    self.value = c
+    self:bang()
+  end
+end
+
+function Number:delta(d)
+  self:set(self:get() + d)
+end
+
+function Number:set_default()
+  self:set(self.default)
+end
+
+function Number:bang()
+  self:action(self:get())
+end
+
+function Number:string()
+  return self.value
+end
+
+
+return Number
