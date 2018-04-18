cb89eadd4b1096c094f10f046374965060f9d0dc
Anton Hornquist <anton.hornquist@gmail.com>
2018-04-18 16:44:50 -0400

    add copy, refactor warps and default specs

--- lua/controlspec.lua
+++ lua/controlspec.lua
@@ -1,5 +1,20 @@
-local WARP_LIN = 1
-local WARP_EXP = 2
+local LinearWarp = {}
+function LinearWarp.map(spec, value)
+  return util.linlin(0, 1, spec.minval, spec.maxval, value)
+end
+
+function LinearWarp.unmap(spec, value)
+  return util.linlin(spec.minval, spec.maxval, 0, 1, value)
+end
+
+local ExponentialWarp = {}
+function ExponentialWarp.map(spec, value)
+  return util.linexp(0, 1, spec.minval, spec.maxval, value)
+end
+
+function ExponentialWarp.unmap(spec, value)
+  return util.explin(spec.minval, spec.maxval, 0, 1, value)
+end
 
 local ControlSpec = {}
 ControlSpec.__index = ControlSpec
@@ -10,39 +25,58 @@ function ControlSpec.new(minval, maxval, warp, step, default, units)
   s.maxval = maxval
   if type(warp) == "string" then
     if warp == 'exp' then
-      s.warp = WARP_EXP
+      s.warp = ExponentialWarp
     else
-      s.warp = WARP_LIN
+      s.warp = LinearWarp
     end
-  elseif type(warp) == "number" then
-    s.warp = warp -- TODO: assumes number is in [WARP_LIN, WARP_EXP]
   else
-    s.warp = WARP_LIN
+    s.warp = LinearWarp
   end
   s.step = step or 0
-  s.default = default or minval -- TODO: test to ensure minval fallback works
+  s.default = default or minval
   s.units = units or ""
   return s
 end
 
+function ControlSpec:cliphi()
+  return math.max(self.minval, self.maxval)
+end
+
+function ControlSpec:cliplo()
+  return math.min(self.minval, self.maxval)
+end
+
 function ControlSpec:map(value)
-  if self.warp == WARP_LIN then
-    return util.linlin(0, 1, self.minval, self.maxval, value)
-  elseif self.warp == WARP_EXP then
-    return util.linexp(0, 1, self.minval, self.maxval, value)
-  end
+  local clamped = util.clamp(value, 0, 1)
+  return util.round(self.warp.map(self, clamped), self.step)
 end
 
 function ControlSpec:unmap(value)
-  if self.warp == WARP_LIN then
-    return util.linlin(self.minval, self.maxval, 0, 1, value)
-  elseif self.warp == WARP_EXP then
-    return util.explin(self.minval, self.maxval, 0, 1, value)
-  end
+  local clamped = util.clamp(util.round(value, self.step), self:cliplo(), self:cliphi())
+  return self.warp.unmap(self, clamped)
 end
 
 function ControlSpec:constrain(value)
-  return util.round(util.clamp(value, self.minval, self.maxval), self.step or 0)
+  return util.round(util.clamp(value, self:cliplo(), self:cliphi()), self.step)
+end
+
+function ControlSpec:range()
+  return maxval - minval
+end
+
+function ControlSpec:ratio()
+  return maxval / minval
+end
+
+function ControlSpec:copy()
+  local s = setmetatable({}, ControlSpec)
+  s.minval = self.minval
+  s.maxval = self.maxval
+  s.warp = self.warp
+  s.step = self.step
+  s.default = self.default
+  s.units = self.units
+  return s
 end
 
 function ControlSpec:print()
@@ -52,80 +86,24 @@ function ControlSpec:print()
   end
 end
 
-function ControlSpec.unipolar()
-  return ControlSpec.new(0, 1, 'lin', 0, 0, "")
-end
-
-function ControlSpec.bipolar()
-  return ControlSpec.new(-1, 1, 'lin', 0, 0, "")
-end
-
-function ControlSpec.freq()
-  return ControlSpec.new(20, 20000, 'exp', 0, 440, "Hz")
-end
-
-function ControlSpec.lofreq()
-  return ControlSpec.new(0.1, 100, 'exp', 0, 6, "Hz")
-end
-
-function ControlSpec.midfreq()
-  return ControlSpec.new(25, 4200, 'exp', 0, 440, "Hz")
-end
-
-function ControlSpec.widefreq()
-  return ControlSpec.new(0.1, 20000, 'exp', 0, 440, "Hz")
-end
-
-function ControlSpec.phase()
-  return ControlSpec.new(0, math.pi, 'lin', 0, 0, "")
-end
-
-function ControlSpec.rq()
-  return ControlSpec.new(0.001, 2, 'exp', 0, 0.707, "")
-end
-
-function ControlSpec.midi()
-  return ControlSpec.new(0, 127, 'lin', 0, 64, "")
-end
-
-function ControlSpec.midinote()
-  return ControlSpec.new(0, 127, 'lin', 0, 60, "")
-end
-
-function ControlSpec.midivelocity()
-  return ControlSpec.new(1, 127, 'lin', 0, 64, "")
-end
-
-function ControlSpec.db()
-  return ControlSpec.new(-60, 0, 'lin', nil, nil, "dB") -- TODO: this uses DbFaderWarp in SuperCollider, would be good to have in lua too
-end
-
-function ControlSpec.amp()
-  return ControlSpec.new(0, 1, 'lin', 0, 0, "") -- TODO: this uses FaderWarp in SuperCollider, would be good to have in lua too
-end
-
-function ControlSpec.boostcut()
-  return ControlSpec.new(-20, 20, 'lin', 0, 0, "dB")
-end
-
-function ControlSpec.pan()
-  return ControlSpec.new(-1, 1, 'lin', 0, 0, "")
-end
-
-function ControlSpec.detune()
-  return ControlSpec.new(-20, 20, 'lin', 0, 0, "Hz")
-end
-
-function ControlSpec.rate()
-  return ControlSpec.new(0.125, 8, 'exp', 0, 1, "")
-end
-
-function ControlSpec.beats()
-  return ControlSpec.new(0, 20, 'lin', 0, 0, "Hz")
-end
-
-function ControlSpec.delay()
-  return ControlSpec.new(0.0001, 1, 'exp', 0, 0.3, "Hz")
-end
+ControlSpec.UNIPOLAR = ControlSpec.new(0, 1, 'lin', 0, 0, "")
+ControlSpec.BIPOLAR = ControlSpec.new(-1, 1, 'lin', 0, 0, "")
+ControlSpec.FREQ = ControlSpec.new(20, 20000, 'exp', 0, 440, "Hz")
+ControlSpec.LOFREQ = ControlSpec.new(0.1, 100, 'exp', 0, 6, "Hz")
+ControlSpec.MIDFREQ = ControlSpec.new(25, 4200, 'exp', 0, 440, "Hz")
+ControlSpec.WIDEFREQ = ControlSpec.new(0.1, 20000, 'exp', 0, 440, "Hz")
+ControlSpec.PHASE = ControlSpec.new(0, math.pi, 'lin', 0, 0, "")
+ControlSpec.RQ = ControlSpec.new(0.001, 2, 'exp', 0, 0.707, "")
+ControlSpec.MIDI = ControlSpec.new(0, 127, 'lin', 0, 64, "")
+ControlSpec.MIDINOTE = ControlSpec.new(0, 127, 'lin', 0, 60, "")
+ControlSpec.MIDIVELOCITY = ControlSpec.new(1, 127, 'lin', 0, 64, "")
+ControlSpec.DB = ControlSpec.new(-60, 0, 'lin', nil, nil, "dB") -- TODO: this uses \db warp == DbFaderWarp in SuperCollider, would be good to have in lua too
+ControlSpec.AMP = ControlSpec.new(0, 1, 'lin', 0, 0, "") -- TODO: this uses \amp warp == FaderWarp in SuperCollider, would be good to have in lua too
+ControlSpec.BOOSTCUT = ControlSpec.new(-20, 20, 'lin', 0, 0, "dB")
+ControlSpec.PAN = ControlSpec.new(-1, 1, 'lin', 0, 0, "")
+ControlSpec.DETUNE = ControlSpec.new(-20, 20, 'lin', 0, 0, "Hz")
+ControlSpec.RATE = ControlSpec.new(0.125, 8, 'exp', 0, 1, "")
+ControlSpec.BEATS = ControlSpec.new(0, 20, 'lin', 0, 0, "Hz")
+ControlSpec.DELAY = ControlSpec.new(0.0001, 1, 'exp', 0, 0.3, "Hz")
 
 return ControlSpec
