434f0233d8ee319ba4589716d57031eb3d682aed
emb <emb@catfact.net>
2018-04-20 18:12:55 -0400

    fix SC install script so it doesn't fail all the time and doesn't copypaste locations

--- sc/install.sh
+++ sc/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-norns_ext_dir=~/.local/share/SuperCollider/Extensions/norns
+sc_ext_dir=~/.local/share/SuperCollider/Extensions
+norns_ext_dir=$sc_ext_dir/norns
 rm -rf $norns_ext_dir
-mkdir -p $norns_ext_dir
+mkdir $norns_ext_dir
+
 
 link() {
     base=`basename $1`
     src=`pwd`/$1
     dst=$norns_ext_dir/$base
-    echo $dst
     ln -sf $src $dst
 }
 
@@ -21,5 +22,8 @@ for file in $(find ../build/sc -name '*.so'); do
 done
 
 
-mkdir ~/.local/share/SuperCollider/Extensions/sc
-ln -sf ~/dust/sc ~/.local/share/SuperCollider/Extensions/sc
+# user extensions supplied by "dust"
+dust_ext_dir=$sc_ext_dir/dust
+rm -rf $dust_ext_dir
+mkdir $dust_ext_dir
+ln -sf ~/dust/sc $dust_ext_dir
