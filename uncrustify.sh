e42f31e46589a4e6042bb3bac59b34436e3f7d51
Artem Popov <artfwo@gmail.com>
2018-05-03 03:10:48 +0700

    add sp_after_comma

--- uncrustify.sh
+++ uncrustify.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # run this from top-level norns directory before committing...
-find ipc-wrapper/src -regex .*[\.][ch] | xargs uncrustify -c uncrustify.cfg --no-backup
-find maiden/src -regex .*[\.][ch] | xargs uncrustify -c uncrustify.cfg --no-backup
+find ws-wrapper/src -regex .*[\.][ch] | xargs uncrustify -c uncrustify.cfg --no-backup
 find matron/src -regex .*[\.][ch] | xargs uncrustify -c uncrustify.cfg --no-backup
