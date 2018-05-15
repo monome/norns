497cabe8d1baeb77459faec70ae44148cfcb8364
Artem Popov <artfwo@gmail.com>
2018-05-15 23:37:49 +0700

    add taper parameter type

---
+++ lua/params/taper.lua
@@ -0,0 +1,82 @@
+--- Taper class
+-- non-linear parameter using @catfact's taper function
+-- @module taper
+
+local util = require 'util'
+
+local Taper = {}
+Taper.__index = Taper
+
+local tTAPER = 5
+
+local function map(x, from_min, from_max, to_min, to_max)
+  return (x - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
+end
+
+function Taper.new(name, min, max, default, k, units)
+  local p = setmetatable({}, Taper)
+  p.t = tTAPER
+  p.name = name
+  p.min = min or 0
+  p.max = max or 1
+  p.k = k or 0
+  p.action = function() end
+  p.default = default or min
+  p.units = units or ""
+  p:set(p.default)
+  return p
+end
+
+function Taper:get()
+  local result
+
+  if self.k == 0 then
+    result = self.value
+  else
+    result = (math.exp(self.value * self.k) - 1) / (math.pow(math.exp(1), self.k) - 1)
+  end
+
+  return map(result, 0, 1, self.min, self.max)
+end
+
+function Taper:get_raw()
+  return self.value
+end
+
+function Taper:set(v)
+  local raw
+  raw = map(v, self.min, self.max, 0, 1)
+
+  if self.k ~= 0 then
+    raw = math.log(raw * (math.pow(math.exp(1), self.k) - 1) + 1) / self.k
+  end
+
+  self:set_raw(raw)
+end
+
+function Taper:set_raw(v)
+  if self.value ~= v then
+    self.value = v
+    self:bang()
+  end
+end
+
+function Taper:delta(d)
+  self:set_raw(self.value + d / 200)
+end
+
+function Taper:set_default()
+  self:set(self.default)
+end
+
+function Taper:bang()
+  if self.value ~= nil then
+    self.action(self.value)
+  end
+end
+
+function Taper:string()
+  return util.round(self:get(), 0.001).." "..self.units
+end
+
+return Taper
