49ac51d3fddd2cce6a866996ff1d85cf2e60e4e8
ezra buchla <emb@catfact.net>
2019-10-18 22:56:31 -0700

    copy 1 less byte with strncpy and let it null-terminate. fixes #925, probably

--- matron/src/system_cmd.c
+++ matron/src/system_cmd.c
@@ -85,11 +85,13 @@ void *run_cmd(void *cmd) {
     fprintf(stderr, "finished line loop; full buffer: \n");    
     fprintf(stderr, "%s\n", capture);
 #endif
-    
-    size_t len = strlen(capture);
+
+    // strncpy adds null terminator, *unless* src length >= nbytes.
+    // this is atually fine, but new gcc throws warning
+    // so, copy 1 less byte and let strncpy add the null instead of doing it ourselves/
+    size_t len = strlen(capture) - 1;
     char *cap = malloc((len + 1) * sizeof(char));
     strncpy(cap, capture, len);
-    cap[len] = '\0';
     union event_data *ev = event_data_new(EVENT_SYSTEM_CMD);
 
     // this will get freed when the event is handled
