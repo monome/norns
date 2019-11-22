f6bcc38e5272fdf3ccca68c71c9d534f7d0ec7bb
brian crabtree <tehn@monome.org>
2019-11-23 05:07:10 +0700

    cleanup, fix resets

--- lua/core/menu/update.lua
+++ lua/core/menu/update.lua
@@ -3,7 +3,20 @@ local m = {
   version = ''
 }
 
-local function checked(result)
+local function checked() print("what??") end
+local function get_update_2() end
+
+local function check_newest()
+  print("checking for update")
+  norns.system_cmd( [[curl -s \
+      https://api.github.com/repos/monome/norns/releases/latest \
+      | grep "browser_download_url.*" \
+      | cut -d : -f 2,3 \
+      | tr -d \"]],
+      checked)
+end
+
+checked = function(result)
   m.url = result
   print(m.url)
   m.url = string.gsub(m.url,"\n","")
@@ -19,17 +32,31 @@ local function checked(result)
 end
 
 
-local function check_newest()
-  print("checking for update")
-  norns.system_cmd( [[curl -s \
-      https://api.github.com/repos/monome/norns/releases/latest \
-      | grep "browser_download_url.*" \
-      | cut -d : -f 2,3 \
-      | tr -d \"]],
-      checked)
+local function get_update()
+  m.message = "preparing..."
+  _menu.redraw()
+  pcall(cleanup) -- shut down script
+  norns.script.clear()
+  print("shutting down audio...")
+  os.execute("sudo systemctl stop norns-jack.service") -- disable audio
+  print("clearing old updates...")
+  os.execute("sudo rm -rf /home/we/update/*") -- clear old updates
+  m.message = "downloading..."
+  _menu.redraw()
+  print("starting download...")
+  local cmd = "wget -T 180 -q -P /home/we/update/ " .. m.url
+  print("> "..cmd)
+  norns.system_cmd(cmd, get_update_2) --download
+  _menu.timer.time = 0.5
+  _menu.timer.count = -1
+  _menu.timer.event = function()
+    m.blink = m.blink == false
+    _menu.redraw()
+  end
+  _menu.timer:start()
 end
 
-local function get_update_2()
+get_update_2 = function()
   _menu.timer:stop()
   m.message = "unpacking update..."
   _menu.redraw()
@@ -55,32 +82,6 @@ local function get_update_2()
 end
 
 
-local function get_update()
-  m.message = "preparing..."
-  _menu.redraw()
-  pcall(cleanup) -- shut down script
-  norns.script.clear()
-  print("shutting down audio...")
-  os.execute("sudo systemctl stop norns-jack.service") -- disable audio
-  print("clearing old updates...")
-  os.execute("sudo rm -rf /home/we/update/*") -- clear old updates
-  m.message = "downloading..."
-  _menu.redraw()
-  print("starting download...")
-  local cmd = "wget -T 180 -q -P /home/we/update/ " .. m.url
-  print("> "..cmd)
-  m.blink = true
-  norns.system_cmd(cmd, get_update_2) --download
-  _menu.timer.time = 1
-  _menu.timer.count = -1
-  _menu.timer.event = function()
-    m.blink = m.blink == false
-    _menu.redraw()
-  end
-  _menu.timer:start()
-  
-end
-
 m.key = function(n,z)
   if m.stage=="init" and z==1 then
     _menu.set_page("SYSTEM")
@@ -92,6 +93,20 @@ m.key = function(n,z)
     elseif n==3 and z==1 then
       m.stage="update"
       get_update()
+    end
+  elseif m.stage=="update" then
+    if n==2 and z==1 then
+      m.stage="cancel"
+      _menu.redraw()
+    end
+  elseif m.stage=="cancel" then
+    if n==2 and z==1 then
+      m.stage="update"
+      _menu.redraw()
+    elseif n==3 and z==1 then
+      os.execute("sudo systemctl restart norns-jack.service")
+      os.execute("sudo systemctl restart norns-sclang.service")
+      os.execute("sudo systemctl restart norns-matron.service")
     end
   elseif m.stage=="done" and z==1 then
     print("shutting down.")
@@ -108,14 +123,16 @@ m.redraw = function()
   screen.clear()
   screen.level(15)
   screen.move(64,40)
-  if m.stage == "init" then
-    screen.text_center(m.message)
-  elseif m.stage == "confirm" then
+  if m.stage == "confirm" then
     screen.text_center("update found: "..m.version)
     screen.move(64,50)
     screen.text_center("install?")
   elseif m.stage == "update" then
     screen.level(m.blink == true and 15 or 3)
+    screen.text_center(m.message)
+  elseif m.stage == "cancel" then
+    screen.text_center("cancel?")
+  else
     screen.text_center(m.message)
   end
   screen.update()
