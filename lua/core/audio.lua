e743b683c372c6b39fdf5c6fd4a09fcab5fbde59
brian crabtree <tehn@monome.org>
2019-12-04 07:39:12 +0700

    audio headings and id/name

--- lua/core/audio.lua
+++ lua/core/audio.lua
@@ -256,92 +256,88 @@ end
 
 function Audio.add_params()
   local cs_MAIN_LEVEL = cs.new(-math.huge,0,'db',0,0,"dB")
-  params:add_group("levels",9)
-  params:add_control("output", "output", cs_MAIN_LEVEL)
-  params:set_action("output",
+  params:add_group("LEVELS",9)
+  params:add_control("output_level", "output", cs_MAIN_LEVEL)
+  params:set_action("output_level",
     function(x) audio.level_dac(util.dbamp(x)) end)
-  params:add_control("input", "input", cs_MAIN_LEVEL)
-  params:set_action("input",
+  params:add_control("input_level", "input", cs_MAIN_LEVEL)
+  params:set_action("input_level",
     function(x) audio.level_adc(util.dbamp(x)) end)
   local cs_MUTE_LEVEL = cs.new(-math.huge,0,'db',0,-math.huge,"dB")
-  params:add_control("monitor", "monitor", cs_MUTE_LEVEL)
-  params:set_action("monitor",
+  params:add_control("monitor_level", "monitor", cs_MUTE_LEVEL)
+  params:set_action("monitor_level",
     function(x) audio.level_monitor(util.dbamp(x)) end)
-  params:add_control("engine", "engine", cs_MAIN_LEVEL)
-  params:set_action("engine",
+  params:add_control("engine_level", "engine", cs_MAIN_LEVEL)
+  params:set_action("engine_level",
     function(x) audio.level_eng(util.dbamp(x)) end)
-  params:add_control("softcut", "softcut", cs_MAIN_LEVEL)
-  params:set_action("softcut",
+  params:add_control("softcut_level", "softcut", cs_MAIN_LEVEL)
+  params:set_action("softcut_level",
     function(x) audio.level_cut(util.dbamp(x)) end)
-  params:add_control("tape", "tape", cs_MUTE_LEVEL)
-  params:set_action("tape",
+  params:add_control("tape_level", "tape", cs_MUTE_LEVEL)
+  params:set_action("tape_level",
     function(x) audio.level_tape(util.dbamp(x)) end)
-  params:add_separator()
+  params:add_separator("HEADPHONE")
   params:add_option("monitor_mode", "monitor mode", {"STEREO", "MONO"})
   params:set_action("monitor_mode",
     function(x)
       if x == 1 then audio.monitor_stereo()
       else audio.monitor_mono() end
     end)
-  params:add_number("headphone", "headphone", 0, 63, 40)
-  params:set_action("headphone",
+  params:add_number("headphone_gain", "gain", 0, 63, 40)
+  params:set_action("headphone_gain",
     function(x) audio.headphone_gain(x) end)
   
-  params:add_group("reverb",11)
+  params:add_group("REVERB",11)
   params:add_option("reverb", "reverb", {"OFF", "ON"}, 2)
   params:set_action("reverb",
   function(x)
-    if x == 1 then
-      audio.rev_off()
-    else
-      audio.rev_on()
-    end
+    if x == 1 then audio.rev_off() else audio.rev_on() end
   end)
   local cs_DB_LEVEL = cs.new(-math.huge,18,'db',0,0,"dB")
   local cs_DB_LEVEL_MUTE = cs.new(-math.huge,18,'db',0,-math.huge,"dB")
   local cs_DB_LEVEL_9DB = cs.new(-math.huge,18,'db',0,-9,"dB")
 
-  params:add_control("rev_eng_input", "rev eng input", cs_DB_LEVEL_9DB)
+  params:add_control("rev_eng_input", "engine input", cs_DB_LEVEL_9DB)
   params:set_action("rev_eng_input",
   function(x) audio.level_eng_rev(util.dbamp(x)) end)
 
-  params:add_control("rev_cut_input", "rev cut input", cs_DB_LEVEL_9DB)
+  params:add_control("rev_cut_input", "softcut input", cs_DB_LEVEL_9DB)
   params:set_action("rev_cut_input",
   function(x) audio.level_cut_rev(util.dbamp(x)) end)
 
-  params:add_control("rev_monitor_input", "rev monitor input", cs_DB_LEVEL_MUTE)
+  params:add_control("rev_monitor_input", "monitor input", cs_DB_LEVEL_MUTE)
   params:set_action("rev_monitor_input",
   function(x) audio.level_monitor_rev(util.dbamp(x)) end)
 
-  params:add_control("rev_tape_input", "rev tape input", cs_DB_LEVEL_MUTE)
+  params:add_control("rev_tape_input", "tape input", cs_DB_LEVEL_MUTE)
   params:set_action("rev_tape_input",
   function(x) audio.level_tape_rev(util.dbamp(x)) end)
 
-  params:add_control("rev_return_level", "rev return level", cs_DB_LEVEL)
+  params:add_control("rev_return_level", "return level", cs_DB_LEVEL)
   params:set_action("rev_return_level",
   function(x) audio.level_rev_dac(util.dbamp(x)) end)
 
 
   local cs_IN_DELAY = cs.new(20,100,'lin',0,60,'ms')
-  params:add_control("rev_pre_delay", "rev pre delay", cs_IN_DELAY)
+  params:add_control("rev_pre_delay", "pre delay", cs_IN_DELAY)
   params:set_action("rev_pre_delay",
   function(x) audio.rev_param("pre_del",x) end)
 
   local cs_LF_FC = cs.new(50,1000,'exp',0, 200,'hz')
-  params:add_control("rev_lf_fc", "rev lf fc", cs_LF_FC)
+  params:add_control("rev_lf_fc", "lf fc", cs_LF_FC)
   params:set_action("rev_lf_fc",
   function(x) audio.rev_param("lf_fc",x) end)
 
   local cs_RT60 = cs.new(0.1,16,'lin',0,6,'s')
-  params:add_control("rev_low_time", "rev low time", cs_RT60)
+  params:add_control("rev_low_time", "low time", cs_RT60)
   params:set_action("rev_low_time",
   function(x) audio.rev_param("low_rt60",x) end)
-  params:add_control("rev_mid_time", "rev mid time", cs_RT60)
+  params:add_control("rev_mid_time", "mid time", cs_RT60)
   params:set_action("rev_mid_time",
   function(x) audio.rev_param("mid_rt60",x) end)
 
   local cs_HF_DAMP = cs.new(1500,20000,'exp',0,6000,'hz')
-  params:add_control("rev_hf_damping", "rev hf damping", cs_HF_DAMP)
+  params:add_control("rev_hf_damping", "hf damping", cs_HF_DAMP)
   params:set_action("rev_hf_damping",
   function(x) audio.rev_param("hf_damp",x) end)
   --[[
@@ -367,7 +363,7 @@ function Audio.add_params()
   function(x) audio.aux_param("level",x) end)
   --]]
 
-  params:add_group("compressor",8)
+  params:add_group("COMPRESSOR",8)
   params:add_option("compressor", "compressor", {"OFF", "ON"})
   params:set_action("compressor",
   function(x)
@@ -378,36 +374,36 @@ function Audio.add_params()
     end
   end)
   local cs_params = cs.new(0,1,'lin',0,1,'')
-  params:add_control("comp_mix", "comp mix", cs_MIX)
+  params:add_control("comp_mix", "mix", cs_MIX)
   params:set_action("comp_mix",
   function(x) audio.comp_mix(x) end)
 
   local cs_RATIO = cs.new(1,20,'lin',0,4,'')
-  params:add_control("comp_ratio", "comp ratio", cs_RATIO)
+  params:add_control("comp_ratio", "ratio", cs_RATIO)
   params:set_action("comp_ratio",
   function(x) audio.comp_param("ratio",x) end)
 
   local cs_THRESH = cs.new(-100,10,'db',0,-18,'dB')
-  params:add_control("comp_threshold", "comp threshold", cs_THRESH)
+  params:add_control("comp_threshold", "threshold", cs_THRESH)
   params:set_action("comp_threshold",
   function(x) audio.comp_param("threshold",x) end)
 
   local cs_ATTACK = cs.new(1,1000,'exp',0,5,'ms')
-  params:add_control("comp_attack", "comp attack", cs_ATTACK)
+  params:add_control("comp_attack", "attack", cs_ATTACK)
   params:set_action("comp_attack",
   function(x) audio.comp_param("attack", x*0.001) end)
   local cs_RELEASE = cs.new(1,1000,'exp',0,50,'ms')
-  params:add_control("comp_release", "comp release", cs_RELEASE)
+  params:add_control("comp_release", "release", cs_RELEASE)
   params:set_action("comp_release",
   function(x) audio.comp_param("release",x * 0.001) end)
 
   local cs_PREGAIN = cs.new(-20,60,'db',0,0,'dB')
-  params:add_control("comp_pre_gain", "comp pre gain", cs_PREGAIN)
+  params:add_control("comp_pre_gain", "pre gain", cs_PREGAIN)
   params:set_action("comp_pre_gain",
   function(x) audio.comp_param("gain_pre",x) end)
 
   local cs_POSTGAIN = cs.new(-20,60,'db',0,9,'dB')
-  params:add_control("comp_post_gain", "comp post gain", cs_POSTGAIN)
+  params:add_control("comp_post_gain", "post gain", cs_POSTGAIN)
   params:set_action("comp_post_gain",
   function(x) audio.comp_param("gain_post",x) end)
 end
