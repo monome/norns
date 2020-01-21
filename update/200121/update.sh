11c2c05504dbe7337df063d40b1a9f4450dfe65b
brian crabtree <tehn@monome.org>
2020-01-21 22:19:50 +0700

    200121

--- update/200121/update.sh
+++ update/200121/update.sh
@@ -50,6 +50,7 @@ find ~/dust -name ._.DS_Store -delete
 
 # kernel
 sudo rm /boot/kernel-*
+sudo find /lib/modules/* -maxdepth 0 -path "/lib/modules/$(uname -r)" -prune -o -type d -delete
 sudo cp -r kernel/boot /
 sudo cp -r kernel/lib /
 HW=$(sudo cat /sys/firmware/devicetree/base/model)
