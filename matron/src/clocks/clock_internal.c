ec22190c32ab55d4383d69cb181c73c36d78c666
Artem Popov <artfwo@gmail.com>
2020-04-01 23:01:24 +0200

    start and stop methods for internal clock

--- matron/src/clocks/clock_internal.c
+++ matron/src/clocks/clock_internal.c
@@ -11,11 +11,11 @@
 static pthread_t clock_internal_thread;
 static double interval_seconds;
 static uint64_t interval_nseconds;
+static double beat;
 
 static void *clock_internal_run(void *p) {
     (void) p;
     struct timespec req;
-    int beat = 0;
 
     while (true) {
         clock_gettime(CLOCK_MONOTONIC, &req);
@@ -28,17 +28,18 @@ static void *clock_internal_run(void *p) {
 
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &req, NULL);
 
-        beat += 1;
+        beat += 1.0;
         clock_update_reference_from(beat, interval_seconds, CLOCK_SOURCE_INTERNAL);
     }
 
     return NULL;
 }
 
-void clock_internal_start() {
+void clock_internal_init() {
     pthread_attr_t attr;
 
     clock_internal_set_tempo(120);
+    beat = 0.0;
 
     pthread_attr_init(&attr);
     pthread_create(&clock_internal_thread, &attr, &clock_internal_run, NULL);
@@ -47,4 +48,13 @@ void clock_internal_start() {
 void clock_internal_set_tempo(double bpm) {
     interval_seconds = 60.0 / bpm;
     interval_nseconds = (uint64_t) (interval_seconds * 1000000000);
+}
+
+void clock_internal_start() {
+    beat = 0.0;
+    clock_start_from(CLOCK_SOURCE_INTERNAL);
+}
+
+void clock_internal_stop() {
+    clock_stop_from(CLOCK_SOURCE_INTERNAL);
 }
