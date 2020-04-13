c8dd716fa12c6aa34a5c917480d7cc33578c0f6b
brian crabtree <tehn@monome.org>
2020-04-14 04:21:58 +0700

    clock.get_beat_ms

--- lua/core/clock.lua
+++ lua/core/clock.lua
@@ -111,6 +111,11 @@ clock.get_tempo = function()
   return _norns.clock_get_tempo()
 end
 
+clock.get_beat_ms = function(x)
+  x = x or 1
+  return (60000.0 / _norns.clock_get_tempo() * x)
+end
+
 
 clock.transport = {}
 
