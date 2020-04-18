e31a9e105807327a85619cbdb4ec3b30b99107d0
brian crabtree <tehn@monome.org>
2020-04-18 10:54:50 -0400

    filter crow/midi input (#1089)

--- matron/src/clocks/clock_crow.c
+++ matron/src/clocks/clock_crow.c
@@ -11,48 +11,48 @@ static double clock_crow_last_time;
 static double duration_buf[DURATION_BUFFER_LENGTH] = {0};
 static uint8_t beat_duration_buf_pos = 0;
 static uint8_t beat_duration_buf_len = 0;
+static double mean_sum;
+static double mean_scale;
 
 #define CLOCK_CROW_DIV 4.0
 
 void clock_crow_init() {
     clock_crow_counter = 0;
     clock_crow_last_time_set = false;
+    mean_sum = 0;
 }
 
 void clock_crow_handle_clock() {
-    double beat_duration;
-    double beat_duration_buf_sum = 0;
-    double beat_duration_buf_avg;
-    double current_time = clock_gettime_secondsf();
+  double beat_duration;
+  double current_time = clock_gettime_secondsf();
 
-    clock_crow_counter++;
+  if(clock_crow_last_time_set == false) {
+    clock_crow_last_time_set = true;
+    clock_crow_last_time = current_time;
+  } else { 
+    beat_duration = (current_time - clock_crow_last_time) * CLOCK_CROW_DIV;
 
-    // TODO: handle the case when clock tick
-    // are being sent after a long delay
-
-    if (clock_crow_last_time_set) {
-        beat_duration = (current_time - clock_crow_last_time) * CLOCK_CROW_DIV;
-        clock_crow_last_time = current_time;
-        clock_crow_last_time_set = true;
-    } else {
-        beat_duration = 0.5;
-        clock_crow_last_time = current_time;
-        clock_crow_last_time_set = true;
+    if(beat_duration > 4) { // assume clock stopped
+      clock_crow_last_time = current_time;
     }
-
-    if (beat_duration_buf_len < DURATION_BUFFER_LENGTH) {
+    else {
+      if (beat_duration_buf_len < DURATION_BUFFER_LENGTH) {
         beat_duration_buf_len++;
+        mean_scale = 1.0/beat_duration_buf_len;
+      }
+
+      double a = beat_duration * mean_scale;
+      mean_sum = mean_sum + a;
+      mean_sum = mean_sum - duration_buf[beat_duration_buf_pos];
+      duration_buf[beat_duration_buf_pos] = a;
+
+      beat_duration_buf_pos = (beat_duration_buf_pos + 1) % DURATION_BUFFER_LENGTH;
+
+      clock_crow_counter++;
+      clock_crow_last_time = current_time;
+
+      double beat = clock_crow_counter / CLOCK_CROW_DIV;
+      clock_update_reference_from(beat, mean_sum, CLOCK_SOURCE_CROW);
     }
-
-    duration_buf[beat_duration_buf_pos] = beat_duration;
-    beat_duration_buf_pos = (beat_duration_buf_pos + 1) % DURATION_BUFFER_LENGTH;
-
-    for (int i = 0; i < beat_duration_buf_len; i++) {
-        beat_duration_buf_sum += duration_buf[i];
-    }
-
-    beat_duration_buf_avg = beat_duration_buf_sum / beat_duration_buf_len;
-
-    double beat = clock_crow_counter / CLOCK_CROW_DIV;
-    clock_update_reference_from(beat, beat_duration_buf_avg, CLOCK_SOURCE_CROW);
+  }
 }
