d514311ab2e28324ed5360c0c3ec32e69d54f09b
Greg Wuller <greg@afofo.com>
2020-05-18 07:11:12 -0700

    expose configured paths in read only table (#1117)
    - makes `_paths` global table read only
    - adds public `paths` table which exposes _path and state related paths
    - teach tabutil.readonly optional limit which keys are exposed

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -15,10 +15,15 @@ local dust = home..'/dust/code/?.lua;'
 package.path = sys..core..params..lib..softcut..dust..package.path
 -- print('package.path: ' .. package.path)
 
-_path = {}
-_path.home = home
-_path.dust = home..'/dust/'
-_path.code = _path.dust..'code/'
-_path.audio = _path.dust..'audio/'
-_path.tape = _path.audio..'tape/'
-_path.data = _path.dust..'data/'
+-- must be done after package path is set
+local tu = require 'tabutil'
+
+local _p = {}
+_p.home = home
+_p.dust = home..'/dust/'
+_p.code = _p.dust..'code/'
+_p.audio = _p.dust..'audio/'
+_p.tape = _p.audio..'tape/'
+_p.data = _p.dust..'data/'
+
+_path = tu.readonly{ table = _p }
--- lua/core/norns.lua
+++ lua/core/norns.lua
@@ -122,6 +122,15 @@ norns.encoders = require 'core/encoders'
 
 _norns.enc = norns.encoders.process
 
+-- extend paths config table
+paths = {
+  shared = _path,
+  this = tab.readonly{
+    table = norns.state,
+    expose = {'script', 'data', 'path', 'name'}
+  },
+}
+
 -- Error handling.
 norns.scripterror = function(msg) print(msg) end
 norns.try = function(f,msg)
