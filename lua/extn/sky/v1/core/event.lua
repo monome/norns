23288c801bc73cadd5b662c017ae219851e3c50d
Greg Wuller <greg@afofo.com>
2020-07-08 22:33:50 -0700

    sky: ArcDialGesture emit on init events

--- lua/extn/sky/v1/core/event.lua
+++ lua/extn/sky/v1/core/event.lua
@@ -203,6 +203,18 @@ local function is_type(event, event_type)
   return event.type == event_type
 end
 
+local function is_init(event)
+  return event.type == SCRIPT_INIT_EVENT
+end
+
+local function is_redraw(event)
+  return event.type == SCRIPT_REDRAW_EVENT
+end
+
+local function is_cleanup(event)
+  return event.type == SCRIPT_CLEANUP_EVENT
+end
+
 local function is_note(event)
   local t = event.type
   return (t == types.NOTE_ON) or (t == types.NOTE_OFF)
@@ -318,6 +330,9 @@ return {
   starts_with = starts_with,
   bpm_to_sec = bpm_to_sec,
   is_type = is_type,
+  is_init = is_init,
+  is_redraw = is_redraw,
+  is_cleanup = is_cleanup,
   is_note = is_note,
   is_clock = is_clock,
   is_transport = is_transport,
