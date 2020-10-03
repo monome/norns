38c0ddab9f065420911e64b2d5bc11bbe03090e3
Greg Wuller <greg@afofo.com>
2020-10-03 00:36:31 -0700

    dev_midi_start: read events from buffer, running status
    new implementation of dev_midi_start with two
    primary goals, (a) minimize system calls by reading
    all data in a rawmidi device buffer in one call instead
    of reading data one byte at a time, and (b) handle
    midi running status when midi data is being read from
    non-usb device class interfaces.
    
    additional features added include:
    - reporting of xruns on midi input should they occur
    - streaming midi sysex data through to lua via existing
      midi event type

--- matron/src/device/device_midi.c
+++ matron/src/device/device_midi.c
@@ -8,6 +8,8 @@
 #include "device.h"
 #include "device_midi.h"
 
+#define DEV_MIDI_INPUT_BUFFER_SIZE 128
+
 unsigned int dev_midi_port_count(const char *path) {
     int card;
     int alsa_dev;
@@ -92,86 +94,195 @@ void dev_midi_deinit(void *self) {
     snd_rawmidi_close(midi->handle_out);
 }
 
-void *dev_midi_start(void *self) {
-    struct dev_midi *midi = (struct dev_midi *)self;
-    union event_data *ev;
+static inline bool is_status_byte(uint8_t byte) {
+    return (byte & 0xf0) >> 7;
+}
 
-    ssize_t read = 0;
+static inline bool is_status_real_time(uint8_t byte) {
+    return byte >= 0xf8;
+}
+
+static inline uint8_t midi_msg_len(uint8_t status) {
+    uint8_t upper = status & 0xf0;
+
+    // channel voice messages
+    switch (upper) {
+    case 0x80:  // note off
+    case 0x90:  // note on
+    case 0xa0:  // polyphonic key pressure
+    case 0xb0:  // control change
+    case 0xe0:  // pitch bend
+        return 3;
+    case 0xc0:  // program change
+    case 0xd0:  // channel pressure
+        return 2;
+    }
+
+    // system real-time messages
+    switch (status) {
+    case 0xf8:  // timing clock
+    case 0xfa:  // start sequence
+    case 0xfb:  // continue sequence
+    case 0xfc:  // stop sequence
+    case 0xfe:  // active sensing
+    case 0xff:  // system reset
+        return 1;
+    }
+
+    // system common messages
+    switch (status) {
+    case 0xf1:  // midi timing code
+    case 0xf3:  // song select
+        return 2;
+    case 0xf2:  // song position pointer
+        return 3;
+    case 0xf6:  // tune request
+        return 1;
+    }
+
+    // system exclusive messages (variable length)
+    switch (status) {
+    case 0xf0:  // sysex start
+        return 3; // special case, deliver sysex to lua in 3 byte chunks
+    case 0xf7:  // sysex stop
+        return 1; // special case, allow single sysex stop as isolated event
+    }
+
+    // channel mode messages
+    return 2;
+}
+
+typedef struct {
+    uint8_t buffer[DEV_MIDI_INPUT_BUFFER_SIZE];
+
+    uint8_t prior_status;
+    uint8_t prior_len;
+
+    uint8_t msg_buf[3];
+    uint8_t msg_pos;
+    uint8_t msg_len;
+    bool    msg_started;
+    bool    msg_sysex;
+} midi_input_state_t;
+
+static inline void midi_input_msg_post(midi_input_state_t *state, struct dev_midi *midi) {
+    union event_data *ev = event_data_new(EVENT_MIDI_EVENT);
+    ev->midi_event.id = midi->dev.id;
+    ev->midi_event.nbytes = state->msg_len;
+    for (uint8_t n = 0; n < state->msg_len; n++) {
+        ev->midi_event.data[n] = state->msg_buf[n];
+    }
+    event_post(ev);
+}
+
+static inline void midi_input_msg_start(midi_input_state_t *state, uint8_t status) {
+    state->msg_pos = 0;
+    state->msg_len = midi_msg_len(status);
+    state->msg_started = true;
+    state->msg_buf[state->msg_pos++] = status;
+    state->msg_sysex = status == 0xf0;
+
+    // save for running status
+    if (!is_status_real_time(status)) {
+        state->prior_status = status;
+        state->prior_len = state->msg_len;
+    }
+}
+
+static inline void midi_input_msg_end(midi_input_state_t *state) {
+    state->msg_started = false;
+    state->msg_pos = 0;
+}
+
+static inline void midi_input_msg_acc(midi_input_state_t *state, uint8_t byte) {
+    if (!state->msg_started) {
+        if (state->msg_sysex) {
+            // continue to pass sysex through in 3 byte chunks
+            state->msg_started = true;
+            state->msg_pos = 0;
+            state->msg_len = 3;
+        } else {
+            // running status, start a new message
+            state->msg_started = true;
+            state->msg_pos = 0;
+            state->msg_len = state->prior_len;
+            state->msg_buf[state->msg_pos++] = state->prior_status;
+        }
+    }
+
+    state->msg_buf[state->msg_pos++] = byte;
+
+    // terminate sysex
+    if (byte == 0xf7) {
+        state->msg_sysex = false;
+        state->msg_len = state->msg_pos;
+    }
+}
+
+static inline bool midi_input_msg_is_complete(midi_input_state_t *state) {
+    return state->msg_started && (state->msg_len == state->msg_pos);
+}
+
+static inline ssize_t dev_midi_consume_buffer(midi_input_state_t *state, ssize_t size, struct dev_midi *midi) {
+    uint8_t i = 0;
     uint8_t byte = 0;
-    uint8_t msg_buf[256];
-    uint8_t msg_pos = 0;
-    uint8_t msg_len = 0;
 
-    do {
-        read = snd_rawmidi_read(midi->handle_in, &byte, 1);
+    for (i = 0; i < size; i++) {
+        byte = state->buffer[i];
 
         if (byte >= 0xf8) {
             clock_midi_handle_message(byte);
+        }
 
-            ev = event_data_new(EVENT_MIDI_EVENT);
-            ev->midi_event.id = midi->dev.id;
-            ev->midi_event.data[0] = byte;
-            ev->midi_event.nbytes = 1;
-            event_post(ev);
+        if (is_status_byte(byte)) {
+            midi_input_msg_start(state, byte);
         } else {
-            if (byte >= 0x80) {
-                msg_buf[0] = byte;
-                msg_pos = 1;
+            midi_input_msg_acc(state, byte);
+        }
 
-                switch (byte & 0xf0) {
-                case 0x80:
-                case 0x90:
-                case 0xa0:
-                case 0xb0:
-                case 0xe0:
-                case 0xf2:
-                    msg_len = 3;
-                    break;
-                case 0xc0:
-                case 0xd0:
-                    msg_len = 2;
-                    break;
-                case 0xf0:
-                    switch (byte & 0x0f) {
-                    case 0x01:
-                    case 0x03:
-                        msg_len = 2;
-                        break;
-                    case 0x07:
-                        // TODO: properly handle sysex length
-                        msg_len = msg_pos; // sysex end
-                        break;
-                    case 0x00:
-                        msg_len = 0; // sysex start
-                        break;
-                    default:
-                        msg_len = 2;
-                        break;
-                    }
-                    break;
-                default:
-                    msg_len = 2;
-                    break;
-                }
-            } else {
-                msg_buf[msg_pos] = byte;
-                msg_pos += 1;
-            }
+        if (midi_input_msg_is_complete(state)) {
+            midi_input_msg_post(state, midi);
+            midi_input_msg_end(state);
+        }
+    }
 
-            if (msg_pos == msg_len) {
-                ev = event_data_new(EVENT_MIDI_EVENT);
-                ev->midi_event.id = midi->dev.id;
-                ev->midi_event.data[0] = msg_buf[0];
-                ev->midi_event.data[1] = msg_len > 1 ? msg_buf[1] : 0;
-                ev->midi_event.data[2] = msg_len > 2 ? msg_buf[2] : 0;
-                ev->midi_event.nbytes = msg_len;
-                event_post(ev);
+    return i;
+}
 
-                msg_pos = 0;
-                msg_len = 0;
+void *dev_midi_start(void *self) {
+    struct dev_midi *midi = (struct dev_midi *)self;
+    struct dev_common *base = (struct dev_common *)self;
+
+    snd_rawmidi_status_t *status = NULL;
+
+    midi_input_state_t state;
+    ssize_t read = 0;
+    ssize_t xruns;
+
+    if (snd_rawmidi_status_malloc(&status) != 0) {
+        fprintf(stderr, "failed allocating rawmidi status, stopping device: %s\n", base->name);
+        return NULL;
+    }
+
+    do {
+        read = snd_rawmidi_read(midi->handle_in, state.buffer, DEV_MIDI_INPUT_BUFFER_SIZE);
+        if (dev_midi_consume_buffer(&state, read, midi) != read) {
+            fprintf(stderr, "midi inconsistency for device: %s\n", base->name);
+        }
+
+        if (snd_rawmidi_status(midi->handle_in, status) == 0) {
+            xruns = snd_rawmidi_status_get_xruns(status);
+            if (xruns > 0) {
+                fprintf(stderr, "xruns (%d) for midi device: %s\n", xruns, base->name);
+                snd_rawmidi_drop(midi->handle_in);
             }
         }
+
     } while (read > 0);
+
+    if (status) {
+        snd_rawmidi_status_free(status);
+    }
 
     return NULL;
 }
