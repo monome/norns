ec5c5dd23f79c7c10d1afceb15f916012f46f028
brian crabtree <tehn@monome.org>
2020-11-27 22:17:14 +0700

    works w/o sudo

--- matron/src/hardware/platform.c
+++ matron/src/hardware/platform.c
@@ -7,7 +7,7 @@ platform_t p;
 
 void init_platform() {
   if(access("/sys/firmware/devicetree/base/model", F_OK ) != -1) {
-    if(system("sudo cat /sys/firmware/devicetree/base/model | grep 'Compute'")) {
+    if(system("cat /sys/firmware/devicetree/base/model | grep 'Compute'")) {
       p = PLATFORM_PI3;
     } else {
       p = PLATFORM_CM3;
