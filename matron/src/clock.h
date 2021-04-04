e8987611594744a629f675d5ed440f040fc2aa6e
Artem Popov <artfwo@gmail.com>
2021-04-04 22:48:13 +0200

    remove threaded coroutine wakeup scheduler

--- matron/src/clock.h
+++ matron/src/clock.h
@@ -10,18 +10,12 @@ typedef enum {
 } clock_source_t;
 
 void clock_init();
-bool clock_schedule_resume_sleep(int thread_id, double seconds);
-bool clock_schedule_resume_sync(int thread_id, double beats);
 void clock_update_reference(double beats, double beat_duration);
 void clock_update_reference_from(double beats, double beat_duration, clock_source_t source);
 void clock_start_from(clock_source_t source);
 void clock_stop_from(clock_source_t source);
 void clock_set_source(clock_source_t source);
-void clock_cancel_all();
 
 double clock_gettime_beats();
 double clock_gettime_secondsf();
 double clock_get_tempo();
-
-void clock_cancel(int);
-void clock_cancel_coro(int);
