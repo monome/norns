25d5853701df0926a077f41819ef453f0c87cd04
Artem Popov <artfwo@gmail.com>
2021-06-02 07:41:30 +0200

    clang-format

--- matron/src/device/device_monitor.c
+++ matron/src/device/device_monitor.c
@@ -31,25 +31,19 @@
 
 // enumerate unix files to watch
 enum {
-  DEV_FILE_TTY   = 0,
-  DEV_FILE_INPUT = 1,
-  DEV_FILE_SOUND = 2,
-  DEV_FILE_NONE  = 3,
+    DEV_FILE_TTY = 0,
+    DEV_FILE_INPUT = 1,
+    DEV_FILE_SOUND = 2,
+    DEV_FILE_NONE = 3,
 };
 #define DEV_FILE_COUNT 3
 
 struct udev_monitor *mon[DEV_FILE_COUNT];
 
-static const char *dev_file_name[] = {
-  "tty",
-  "input",
-  "sound",
-  "none"
-};
+static const char *dev_file_name[] = {"tty", "input", "sound", "none"};
 
 //-------------------------
 //----- static variables
-
 
 // file descriptors to watch/poll
 struct pollfd pfds[DEV_FILE_COUNT];
@@ -79,329 +73,323 @@ static const char *get_alsa_midi_node(struct udev_device *dev);
 static const char *get_device_name(struct udev_device *dev);
 
 static inline void print_watch_error(const char *msg, int file_idx) {
-  fprintf(stderr, "error: %s on subsystem %s", msg, dev_file_name[file_idx]);
+    fprintf(stderr, "error: %s on subsystem %s", msg, dev_file_name[file_idx]);
 }
 
 //--------------------------------
 //---- extern function definitions
 void dev_monitor_init(void) {
-  struct udev *udev = NULL;
-  pthread_attr_t attr;
-  int s;
+    struct udev *udev = NULL;
+    pthread_attr_t attr;
+    int s;
 
-  udev = udev_new();
-  assert(udev);
+    udev = udev_new();
+    assert(udev);
 
-  for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
-    mon[fidx] = NULL;
-    struct udev_monitor *m = udev_monitor_new_from_netlink(udev, "udev");
-    if (m == NULL) {
-      print_watch_error("couldn't create udev monitor", fidx);
-      continue;
+    for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
+        mon[fidx] = NULL;
+        struct udev_monitor *m = udev_monitor_new_from_netlink(udev, "udev");
+        if (m == NULL) {
+            print_watch_error("couldn't create udev monitor", fidx);
+            continue;
+        }
+        if (udev_monitor_filter_add_match_subsystem_devtype(m, dev_file_name[fidx], NULL) < 0) {
+            print_watch_error("couldn't add subsys filter", fidx);
+            continue;
+        }
+        if (udev_monitor_enable_receiving(m) < 0) {
+            print_watch_error("failed to enable monitor", fidx);
+            continue;
+        }
+        pfds[fidx].fd = udev_monitor_get_fd(m);
+        pfds[fidx].events = POLLIN;
+        mon[fidx] = m;
     }
-    if (udev_monitor_filter_add_match_subsystem_devtype(m, dev_file_name[fidx], NULL) < 0) {
-      print_watch_error("couldn't add subsys filter", fidx);
-      continue;
-    }
-    if (udev_monitor_enable_receiving(m) < 0) {
-      print_watch_error("failed to enable monitor", fidx);
-      continue;
-    }
-    pfds[fidx].fd = udev_monitor_get_fd(m);
-    pfds[fidx].events = POLLIN;
-    mon[fidx] = m;
-  }
 
-  s = pthread_attr_init(&attr);
-  if (s) {
-    fprintf(stderr, "error initializing thread attributes\n");
-  }
-  s = pthread_create(&watch_tid, &attr, watch_loop, NULL);
-  if (s) {
-    fprintf(stderr, "error creating thread\n");
-  }
-  pthread_attr_destroy(&attr);
+    s = pthread_attr_init(&attr);
+    if (s) {
+        fprintf(stderr, "error initializing thread attributes\n");
+    }
+    s = pthread_create(&watch_tid, &attr, watch_loop, NULL);
+    if (s) {
+        fprintf(stderr, "error creating thread\n");
+    }
+    pthread_attr_destroy(&attr);
 }
 
 void dev_monitor_deinit(void) {
-  pthread_cancel(watch_tid);
-  for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
-    if (mon[fidx] != NULL) { 
-      free(mon[fidx]);
+    pthread_cancel(watch_tid);
+    for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
+        if (mon[fidx] != NULL) {
+            free(mon[fidx]);
+        }
     }
-  }
 }
 
 int dev_monitor_scan(void) {
-  struct udev *udev;
-  struct udev_device *dev;
+    struct udev *udev;
+    struct udev_device *dev;
 
-  udev = udev_new();
-  if (udev == NULL) {
-    fprintf(stderr, "device_monitor_scan(): failed to create udev\n");
-    return 1;
-  }
-
-  for (int fidx=0; fidx < DEV_FILE_COUNT; ++fidx) {
-    struct udev_enumerate *ue;
-    struct udev_list_entry *devices, *dev_list_entry;
-
-    ue = udev_enumerate_new(udev);		
-    udev_enumerate_add_match_subsystem(ue, dev_file_name[fidx]);
-    udev_enumerate_scan_devices(ue);
-    devices = udev_enumerate_get_list_entry(ue);
-
-    udev_list_entry_foreach(dev_list_entry, devices) {
-      const char *path;
-
-      path = udev_list_entry_get_name(dev_list_entry);	    
-      dev = udev_device_new_from_syspath(udev, path);
-
-      if (dev != NULL) {
-        if (udev_device_get_parent_with_subsystem_devtype(dev, "usb", NULL)) {
-          add_dev(dev, fidx);
-        }
-      }
-      udev_device_unref(dev);
+    udev = udev_new();
+    if (udev == NULL) {
+        fprintf(stderr, "device_monitor_scan(): failed to create udev\n");
+        return 1;
     }
 
-    udev_enumerate_unref(ue);
-  }
-  return 0;
+    for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
+        struct udev_enumerate *ue;
+        struct udev_list_entry *devices, *dev_list_entry;
+
+        ue = udev_enumerate_new(udev);
+        udev_enumerate_add_match_subsystem(ue, dev_file_name[fidx]);
+        udev_enumerate_scan_devices(ue);
+        devices = udev_enumerate_get_list_entry(ue);
+
+        udev_list_entry_foreach(dev_list_entry, devices) {
+            const char *path;
+
+            path = udev_list_entry_get_name(dev_list_entry);
+            dev = udev_device_new_from_syspath(udev, path);
+
+            if (dev != NULL) {
+                if (udev_device_get_parent_with_subsystem_devtype(dev, "usb", NULL)) {
+                    add_dev(dev, fidx);
+                }
+            }
+            udev_device_unref(dev);
+        }
+
+        udev_enumerate_unref(ue);
+    }
+    return 0;
 }
 
 //-------------------------------
 //--- static function definitions
 
-
 void *watch_loop(void *p) {
-  (void)p;
-  struct udev_device *dev;
+    (void)p;
+    struct udev_device *dev;
 
-  while (1) {
-    if (poll(pfds, DEV_FILE_COUNT, WATCH_TIMEOUT_MS) < 0) {
-      switch (errno) {
-        case EINVAL:
-          perror("error in poll()");
-          exit(1);
-        case EINTR:
-        case EAGAIN:
-          continue;
-      }
-    }
-
-    for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
-      if (pfds[fidx].revents & POLLIN) {
-        dev = udev_monitor_receive_device(mon[fidx]);
-        if (dev) {
-          const char *action = udev_device_get_action(dev);
-          if (action != NULL) {
-            if (strcmp(action, "remove") == 0) {
-              rm_dev(dev, fidx);
-            } else {
-              add_dev(dev, fidx);
-            }			
-          } else {
-            fprintf(stderr, "dev_monitor error: unknown device action\n");
-          }
-          udev_device_unref(dev);
-        } else {
-          fprintf(stderr, "dev_monitor error: no device data\n");
+    while (1) {
+        if (poll(pfds, DEV_FILE_COUNT, WATCH_TIMEOUT_MS) < 0) {
+            switch (errno) {
+            case EINVAL:
+                perror("error in poll()");
+                exit(1);
+            case EINTR:
+            case EAGAIN:
+                continue;
+            }
         }
-      }
+
+        for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
+            if (pfds[fidx].revents & POLLIN) {
+                dev = udev_monitor_receive_device(mon[fidx]);
+                if (dev) {
+                    const char *action = udev_device_get_action(dev);
+                    if (action != NULL) {
+                        if (strcmp(action, "remove") == 0) {
+                            rm_dev(dev, fidx);
+                        } else {
+                            add_dev(dev, fidx);
+                        }
+                    } else {
+                        fprintf(stderr, "dev_monitor error: unknown device action\n");
+                    }
+                    udev_device_unref(dev);
+                } else {
+                    fprintf(stderr, "dev_monitor error: no device data\n");
+                }
+            }
+        }
     }
-  }
 }
 
-void rm_dev(struct udev_device *dev, int dev_file) {     
-  const char *node = udev_device_get_devnode(dev);
-  if (node == NULL ) {
-    return;
-  }
-  switch(dev_file) {
+void rm_dev(struct udev_device *dev, int dev_file) {
+    const char *node = udev_device_get_devnode(dev);
+    if (node == NULL) {
+        return;
+    }
+    switch (dev_file) {
     case DEV_FILE_TTY:
-      rm_dev_tty(dev, node);
-      break;
+        rm_dev_tty(dev, node);
+        break;
     case DEV_FILE_INPUT:
-      dev_list_remove(DEV_TYPE_HID, node);
-      break;
+        dev_list_remove(DEV_TYPE_HID, node);
+        break;
     case DEV_FILE_SOUND:
-      dev_list_remove(DEV_TYPE_MIDI, node);
-      break;
+        dev_list_remove(DEV_TYPE_MIDI, node);
+        break;
     case DEV_FILE_NONE:
-    default:
-      ;;
-  }
+    default:;
+        ;
+    }
 }
 
-void rm_dev_tty(struct udev_device *dev, const char *node) {     
+void rm_dev_tty(struct udev_device *dev, const char *node) {
 #ifdef DEVICE_MONITOR_DEBUG
-  fprintf(stderr, "rm_dev_tty: %s\n", node);
+    fprintf(stderr, "rm_dev_tty: %s\n", node);
 #endif
 
-  if (fnmatch("/dev/ttyUSB*", node, 0) == 0) {
+    if (fnmatch("/dev/ttyUSB*", node, 0) == 0) {
 #ifdef DEVICE_MONITOR_DEBUG
-    fprintf(stderr, "got ttyUSB, assuming grid\n");
+        fprintf(stderr, "got ttyUSB, assuming grid\n");
 #endif
-    dev_list_remove(DEV_TYPE_MONOME, node);
-    return;
-  }
+        dev_list_remove(DEV_TYPE_MONOME, node);
+        return;
+    }
 
-  if (is_dev_monome_grid(dev)) {
+    if (is_dev_monome_grid(dev)) {
 #ifdef DEVICE_MONITOR_DEBUG
-    fprintf(stderr, "tty appears to be ACM grid\n");
+        fprintf(stderr, "tty appears to be ACM grid\n");
 #endif
-    dev_list_remove(DEV_TYPE_MONOME, node);
-    return;
-  }
+        dev_list_remove(DEV_TYPE_MONOME, node);
+        return;
+    }
 
 #ifdef DEVICE_MONITOR_DEBUG
-  fprintf(stderr, "assuming this tty is a crow\n");
+    fprintf(stderr, "assuming this tty is a crow\n");
 #endif
-  dev_list_remove(DEV_TYPE_CROW, node);
+    dev_list_remove(DEV_TYPE_CROW, node);
 }
-
 
 void add_dev(struct udev_device *dev, int fidx) {
 #ifdef DEVICE_MONITOR_DEBUG
-  const char *node = udev_device_get_devnode(dev);
-  fprintf(stderr, "adding device: %s\n", node);
+    const char *node = udev_device_get_devnode(dev);
+    fprintf(stderr, "adding device: %s\n", node);
 #endif
 
-  switch(fidx) {
-    case DEV_FILE_TTY:	 
-      add_dev_tty(dev);
-      break;
+    switch (fidx) {
+    case DEV_FILE_TTY:
+        add_dev_tty(dev);
+        break;
     case DEV_FILE_INPUT:
-      add_dev_input(dev);
-      break;
+        add_dev_input(dev);
+        break;
     case DEV_FILE_SOUND:
-      add_dev_sound(dev);
-      break;
+        add_dev_sound(dev);
+        break;
     case DEV_FILE_NONE:
     default:
-      break;			
-  }
-
+        break;
+    }
 }
 
 void add_dev_tty(struct udev_device *dev) {
-  const char *node = udev_device_get_devnode(dev);
-  const char *device_product_string = udev_device_get_property_value(dev, "ID_MODEL");
-
+    const char *node = udev_device_get_devnode(dev);
+    const char *device_product_string = udev_device_get_property_value(dev, "ID_MODEL");
 
 #ifdef DEVICE_MONITOR_DEBUG
-  fprintf(stderr, "add_dev_tty: %s\n", node);
+    fprintf(stderr, "add_dev_tty: %s\n", node);
 #endif
 
-  if (fnmatch("/dev/ttyUSB*", node, 0) == 0) {
+    if (fnmatch("/dev/ttyUSB*", node, 0) == 0) {
 #ifdef DEVICE_MONITOR_DEBUG
-    fprintf(stderr, "got ttyUSB, assuming grid\n");
+        fprintf(stderr, "got ttyUSB, assuming grid\n");
 #endif
-    dev_list_add(DEV_TYPE_MONOME, node, get_device_name(dev));
-    return;
-  }
+        dev_list_add(DEV_TYPE_MONOME, node, get_device_name(dev));
+        return;
+    }
 
-  if (is_dev_monome_grid(dev)) {
+    if (is_dev_monome_grid(dev)) {
 #ifdef DEVICE_MONITOR_DEBUG
-    fprintf(stderr, "tty appears to be grid-st\n");
+        fprintf(stderr, "tty appears to be grid-st\n");
 #endif
-    dev_list_add(DEV_TYPE_MONOME, node, get_device_name(dev));
-    return;
-  }
+        dev_list_add(DEV_TYPE_MONOME, node, get_device_name(dev));
+        return;
+    }
 
-  if(!strcmp(device_product_string, "crow:_telephone_line")) {
+    if (!strcmp(device_product_string, "crow:_telephone_line")) {
 #ifdef DEVICE_MONITOR_DEBUG
-    fprintf(stderr, "tty is a crow\n");
+        fprintf(stderr, "tty is a crow\n");
 #endif
-    dev_list_add(DEV_TYPE_CROW, node, get_device_name(dev));
-  }
-  else {
-    fprintf(stderr, "device monitor: unmatched tty device\n");
-  }
-
+        dev_list_add(DEV_TYPE_CROW, node, get_device_name(dev));
+    } else {
+        fprintf(stderr, "device monitor: unmatched tty device\n");
+    }
 }
 
 void add_dev_input(struct udev_device *dev) {
-  const char *node = udev_device_get_devnode(dev);
-  dev_list_add(DEV_TYPE_HID, node, get_device_name(dev));
+    const char *node = udev_device_get_devnode(dev);
+    dev_list_add(DEV_TYPE_HID, node, get_device_name(dev));
 }
 
 void add_dev_sound(struct udev_device *dev) {
-  // try to act according to
-  // https://github.com/systemd/systemd/blob/master/rules/78-sound-card.rules
-  const char *alsa_node = get_alsa_midi_node(dev);	 
-  if (alsa_node != NULL) {
-    dev_list_add(DEV_TYPE_MIDI, alsa_node, get_device_name(dev));
-  }
+    // try to act according to
+    // https://github.com/systemd/systemd/blob/master/rules/78-sound-card.rules
+    const char *alsa_node = get_alsa_midi_node(dev);
+    if (alsa_node != NULL) {
+        dev_list_add(DEV_TYPE_MIDI, alsa_node, get_device_name(dev));
+    }
 }
 
 // try to get midi device node from udev_device
 const char *get_alsa_midi_node(struct udev_device *dev) {
-  const char *subsys;
-  const char *syspath;
-  DIR *sysdir;
-  struct dirent *sysdir_ent;
-  int alsa_card, alsa_dev;
-  char *result = NULL;
+    const char *subsys;
+    const char *syspath;
+    DIR *sysdir;
+    struct dirent *sysdir_ent;
+    int alsa_card, alsa_dev;
+    char *result = NULL;
 
-  subsys = udev_device_get_subsystem(dev);
+    subsys = udev_device_get_subsystem(dev);
 
-  if (strcmp(subsys, "sound") == 0) {
-    syspath = udev_device_get_syspath(dev);
-    sysdir = opendir(syspath);
+    if (strcmp(subsys, "sound") == 0) {
+        syspath = udev_device_get_syspath(dev);
+        sysdir = opendir(syspath);
 
-    while ((sysdir_ent = readdir(sysdir)) != NULL) {
-      if (sscanf(sysdir_ent->d_name, "midiC%uD%u", &alsa_card, &alsa_dev) == 2) {
-        if (asprintf(&result, "/dev/snd/%s", sysdir_ent->d_name) < 0) {
-          fprintf(stderr, "failed to create alsa device path for %s\n", sysdir_ent->d_name);
-          return NULL;
+        while ((sysdir_ent = readdir(sysdir)) != NULL) {
+            if (sscanf(sysdir_ent->d_name, "midiC%uD%u", &alsa_card, &alsa_dev) == 2) {
+                if (asprintf(&result, "/dev/snd/%s", sysdir_ent->d_name) < 0) {
+                    fprintf(stderr, "failed to create alsa device path for %s\n", sysdir_ent->d_name);
+                    return NULL;
+                }
+            }
         }
-      }
     }
-  }
 
-  return result;
+    return result;
 }
 
 const char *get_device_name(struct udev_device *dev) {
-  char *current_name = NULL;
-  struct udev_device *current_dev = dev;
+    char *current_name = NULL;
+    struct udev_device *current_dev = dev;
 
-  while (current_name == NULL) {
-    current_name = (char *)udev_device_get_sysattr_value(current_dev, "product");
-    current_dev = udev_device_get_parent(current_dev);
+    while (current_name == NULL) {
+        current_name = (char *)udev_device_get_sysattr_value(current_dev, "product");
+        current_dev = udev_device_get_parent(current_dev);
 
-    if (current_dev == NULL) {
-      break;
+        if (current_dev == NULL) {
+            break;
+        }
     }
-  }
 
-  return strdup(current_name);
+    return strdup(current_name);
 }
 
 int is_dev_monome_grid(struct udev_device *dev) {
-  const char *vendor, *model;
+    const char *vendor, *model;
 
-  vendor = udev_device_get_property_value(dev, "ID_VENDOR");
-  model = udev_device_get_property_value(dev, "ID_MODEL");
+    vendor = udev_device_get_property_value(dev, "ID_VENDOR");
+    model = udev_device_get_property_value(dev, "ID_MODEL");
 
-  if (vendor == NULL || model == NULL) {
+    if (vendor == NULL || model == NULL) {
+        return 0;
+    }
+
+    if (strcmp(vendor, "monome") != 0) {
+        return 0;
+    }
+
+    if (strcmp(model, "grid") == 0) {
+        // a monome grid
+        return 1;
+    }
+    if (strcmp(model, "monome") == 0) {
+        // probably a clone
+        return 1;
+    }
+
     return 0;
-  }
-
-  if (strcmp(vendor, "monome") != 0) {
-    return 0;
-  }
-
-  if (strcmp(model, "grid") == 0) {	    
-    // a monome grid
-    return 1;
-  }
-  if (strcmp(model, "monome") == 0) {
-    // probably a clone
-    return 1;
-  }
-
-  return 0;
 }
--- matron/src/device/device_monome.c
+++ matron/src/device/device_monome.c
@@ -27,186 +27,186 @@ static void dev_monome_handle_encoder_lift(const monome_event_t *e, void *p);
 
 // allocate and initialize a new device
 int dev_monome_init(void *self) {
-  struct dev_monome *md = (struct dev_monome *)self;
-  struct dev_common *base = (struct dev_common *)self;
-  const char *name;
-  const char *serial;
-  monome_t *m;
+    struct dev_monome *md = (struct dev_monome *)self;
+    struct dev_common *base = (struct dev_common *)self;
+    const char *name;
+    const char *serial;
+    monome_t *m;
 
-  m = monome_open(md->dev.path);
+    m = monome_open(md->dev.path);
 
-  if (!m) {
-    fprintf(stderr, "error: couldn't open monome device at %s\n", md->dev.path);
-    return -1;
-  }
+    if (!m) {
+        fprintf(stderr, "error: couldn't open monome device at %s\n", md->dev.path);
+        return -1;
+    }
 
-  md->m = m;
+    md->m = m;
 
-  memset(md->data, 0, sizeof(md->data));
-  memset(md->dirty, 0, sizeof(md->dirty));
+    memset(md->data, 0, sizeof(md->data));
+    memset(md->dirty, 0, sizeof(md->dirty));
 
-  md->rows = monome_get_rows(md->m);
-  md->cols = monome_get_cols(md->m);
+    md->rows = monome_get_rows(md->m);
+    md->cols = monome_get_cols(md->m);
 
-  if (md->rows == 0 && md->cols == 0) {
-    fprintf(stderr, "monome device reporing zero rows/cols; assuming arc\n");
-    md->type = DEVICE_MONOME_TYPE_ARC;
+    if (md->rows == 0 && md->cols == 0) {
+        fprintf(stderr, "monome device reporing zero rows/cols; assuming arc\n");
+        md->type = DEVICE_MONOME_TYPE_ARC;
 
-  } else {
-    md->type = DEVICE_MONOME_TYPE_GRID;
-    md->quads = (md->rows * md->cols) / 64;
-    fprintf(stderr, "monome device appears to be a grid; rows=%d; cols=%d; quads=%d\n",
-        md->rows, md->cols, md->quads);
-  }
+    } else {
+        md->type = DEVICE_MONOME_TYPE_GRID;
+        md->quads = (md->rows * md->cols) / 64;
+        fprintf(stderr, "monome device appears to be a grid; rows=%d; cols=%d; quads=%d\n", md->rows, md->cols,
+                md->quads);
+    }
 
-  monome_register_handler(m, MONOME_BUTTON_DOWN, dev_monome_handle_press, md);
-  monome_register_handler(m, MONOME_BUTTON_UP, dev_monome_handle_lift, md);
-  monome_register_handler(m, MONOME_ENCODER_DELTA, dev_monome_handle_encoder_delta, md);
-  monome_register_handler(m, MONOME_ENCODER_KEY_DOWN, dev_monome_handle_encoder_press, md);
-  monome_register_handler(m, MONOME_ENCODER_KEY_UP, dev_monome_handle_encoder_lift, md);
+    monome_register_handler(m, MONOME_BUTTON_DOWN, dev_monome_handle_press, md);
+    monome_register_handler(m, MONOME_BUTTON_UP, dev_monome_handle_lift, md);
+    monome_register_handler(m, MONOME_ENCODER_DELTA, dev_monome_handle_encoder_delta, md);
+    monome_register_handler(m, MONOME_ENCODER_KEY_DOWN, dev_monome_handle_encoder_press, md);
+    monome_register_handler(m, MONOME_ENCODER_KEY_UP, dev_monome_handle_encoder_lift, md);
 
-  // drop the name set by udev and use libmonome-provided name
-  free(base->name);
-  name = monome_get_friendly_name(m);
-  base->name = strdup(name);
+    // drop the name set by udev and use libmonome-provided name
+    free(base->name);
+    name = monome_get_friendly_name(m);
+    base->name = strdup(name);
 
-  serial = monome_get_serial(m);
-  base->serial = strdup(serial);
+    serial = monome_get_serial(m);
+    base->serial = strdup(serial);
 
-  base->start = &dev_monome_start;
-  base->deinit = &dev_monome_deinit;
+    base->start = &dev_monome_start;
+    base->deinit = &dev_monome_deinit;
 
-  return 0;
+    return 0;
 }
 
 // calculate quadrant number given x/y
 static inline uint8_t dev_monome_quad_idx(uint8_t x, uint8_t y) {
-  return ((y > 7) << 1) | (x > 7);
+    return ((y > 7) << 1) | (x > 7);
 }
 // calcalate offset into quad data given x/y
 static inline uint8_t dev_monome_quad_offset(uint8_t x, uint8_t y) {
-  return ((y & 7) * 8) + (x & 7);
+    return ((y & 7) * 8) + (x & 7);
 }
 
 // set grid rotation
 void dev_monome_set_rotation(struct dev_monome *md, uint8_t rotation) {
-  monome_set_rotation(md->m, rotation);
+    monome_set_rotation(md->m, rotation);
 }
 
 // set a given LED value
 void dev_monome_grid_set_led(struct dev_monome *md, uint8_t x, uint8_t y, uint8_t val) {
-  uint8_t q = dev_monome_quad_idx(x, y);
-  md->data[q][dev_monome_quad_offset(x, y)] = val;
-  md->dirty[q] = true;
+    uint8_t q = dev_monome_quad_idx(x, y);
+    md->data[q][dev_monome_quad_offset(x, y)] = val;
+    md->dirty[q] = true;
 }
 
 // set a given LED value
 void dev_monome_arc_set_led(struct dev_monome *md, uint8_t n, uint8_t x, uint8_t val) {
-  md->data[n & 3][x & 63] = val;
-  md->dirty[n & 3] = true;
+    md->data[n & 3][x & 63] = val;
+    md->dirty[n & 3] = true;
 }
 
 // set all LEDs to value
 void dev_monome_all_led(struct dev_monome *md, uint8_t val) {
-  for (uint8_t q = 0; q < md->quads; q++) {
-    for (uint8_t i = 0; i < 64; i++) {
-      md->data[q][i] = val;
+    for (uint8_t q = 0; q < md->quads; q++) {
+        for (uint8_t i = 0; i < 64; i++) {
+            md->data[q][i] = val;
+        }
+        md->dirty[q] = true;
     }
-    md->dirty[q] = true;
-  }
 }
 
 // transmit all dirty quads
 void dev_monome_refresh(struct dev_monome *md) {
-  static const int quad_xoff[4] = {0, 8, 0, 8};
-  static const int quad_yoff[4] = {0, 0, 8, 8};
+    static const int quad_xoff[4] = {0, 8, 0, 8};
+    static const int quad_yoff[4] = {0, 0, 8, 8};
 
-  if (md->m == NULL) {
-    return;
-  }
-
-  for (int quad = 0; quad < md->quads; quad++) {
-    if (md->dirty[quad]) {
-      if (md->type == DEVICE_MONOME_TYPE_ARC) {
-        monome_led_ring_map(md->m, quad, md->data[quad]);
-      } else {
-        monome_led_level_map(md->m, quad_xoff[quad], quad_yoff[quad], md->data[quad]);
-      }
-      md->dirty[quad] = false;
+    if (md->m == NULL) {
+        return;
     }
-  }
+
+    for (int quad = 0; quad < md->quads; quad++) {
+        if (md->dirty[quad]) {
+            if (md->type == DEVICE_MONOME_TYPE_ARC) {
+                monome_led_ring_map(md->m, quad, md->data[quad]);
+            } else {
+                monome_led_level_map(md->m, quad_xoff[quad], quad_yoff[quad], md->data[quad]);
+            }
+            md->dirty[quad] = false;
+        }
+    }
 }
 
 // intensity
 void dev_monome_intensity(struct dev_monome *md, uint8_t i) {
-  if (i > 15)
-    i = 15;
-  monome_led_intensity(md->m, i);
+    if (i > 15)
+        i = 15;
+    monome_led_intensity(md->m, i);
 }
 
 //--------------------
 //--- static functions
 
 static inline void grid_key_event(const monome_event_t *e, void *p, int state) {
-  struct dev_monome *md = (struct dev_monome *)p;
-  union event_data *ev = event_data_new(EVENT_GRID_KEY);
-  ev->grid_key.id = md->dev.id;
-  ev->grid_key.x = e->grid.x;
-  ev->grid_key.y = e->grid.y;
-  ev->grid_key.state = state;
-  event_post(ev);
+    struct dev_monome *md = (struct dev_monome *)p;
+    union event_data *ev = event_data_new(EVENT_GRID_KEY);
+    ev->grid_key.id = md->dev.id;
+    ev->grid_key.x = e->grid.x;
+    ev->grid_key.y = e->grid.y;
+    ev->grid_key.state = state;
+    event_post(ev);
 }
 
 void dev_monome_handle_press(const monome_event_t *e, void *p) {
-  grid_key_event(e, p, 1);
+    grid_key_event(e, p, 1);
 }
 
 void dev_monome_handle_lift(const monome_event_t *e, void *p) {
-  grid_key_event(e, p, 0);
+    grid_key_event(e, p, 0);
 }
 
 void dev_monome_handle_encoder_delta(const monome_event_t *e, void *p) {
-  struct dev_monome *md = (struct dev_monome *)p;
-  union event_data *ev = event_data_new(EVENT_ARC_ENCODER_DELTA);
-  ev->arc_encoder_delta.id = md->dev.id;
-  ev->arc_encoder_delta.number = e->encoder.number;
-  ev->arc_encoder_delta.delta = e->encoder.delta;
-  event_post(ev);
+    struct dev_monome *md = (struct dev_monome *)p;
+    union event_data *ev = event_data_new(EVENT_ARC_ENCODER_DELTA);
+    ev->arc_encoder_delta.id = md->dev.id;
+    ev->arc_encoder_delta.number = e->encoder.number;
+    ev->arc_encoder_delta.delta = e->encoder.delta;
+    event_post(ev);
 }
 
 void dev_monome_handle_encoder_press(const monome_event_t *e, void *p) {
-  struct dev_monome *md = (struct dev_monome *)p;
-  union event_data *ev = event_data_new(EVENT_ARC_ENCODER_KEY);
-  ev->arc_encoder_key.id = md->dev.id;
-  ev->arc_encoder_key.number = e->encoder.number;
-  ev->arc_encoder_key.state = 1;
-  event_post(ev);
+    struct dev_monome *md = (struct dev_monome *)p;
+    union event_data *ev = event_data_new(EVENT_ARC_ENCODER_KEY);
+    ev->arc_encoder_key.id = md->dev.id;
+    ev->arc_encoder_key.number = e->encoder.number;
+    ev->arc_encoder_key.state = 1;
+    event_post(ev);
 }
 
 void dev_monome_handle_encoder_lift(const monome_event_t *e, void *p) {
-  struct dev_monome *md = (struct dev_monome *)p;
-  union event_data *ev = event_data_new(EVENT_ARC_ENCODER_KEY);
-  ev->arc_encoder_key.id = md->dev.id;
-  ev->arc_encoder_key.number = e->encoder.number;
-  ev->arc_encoder_key.state = 0;
-  event_post(ev);
+    struct dev_monome *md = (struct dev_monome *)p;
+    union event_data *ev = event_data_new(EVENT_ARC_ENCODER_KEY);
+    ev->arc_encoder_key.id = md->dev.id;
+    ev->arc_encoder_key.number = e->encoder.number;
+    ev->arc_encoder_key.state = 0;
+    event_post(ev);
 }
 
 int dev_monome_grid_rows(struct dev_monome *md) {
-  return monome_get_rows(md->m);
+    return monome_get_rows(md->m);
 }
 
 int dev_monome_grid_cols(struct dev_monome *md) {
-  return monome_get_cols(md->m);
+    return monome_get_cols(md->m);
 }
 
 void *dev_monome_start(void *md) {
-  monome_event_loop(((struct dev_monome *)md)->m);
-  return NULL;
+    monome_event_loop(((struct dev_monome *)md)->m);
+    return NULL;
 }
 
 void dev_monome_deinit(void *self) {
-  struct dev_monome *md = (struct dev_monome *)self;
-  monome_close(md->m); // libmonome frees the monome_t pointer
-  md->m = NULL;
+    struct dev_monome *md = (struct dev_monome *)self;
+    monome_close(md->m); // libmonome frees the monome_t pointer
+    md->m = NULL;
 }
