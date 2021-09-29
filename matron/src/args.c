6264827f5cd9d98cf2dd0957649c45efd6269483
Will Winder <wwinder.unh@gmail.com>
2021-09-29 08:16:32 -0400

    Configurable OSC remote port. (#1442)

--- matron/src/args.c
+++ matron/src/args.c
@@ -8,6 +8,7 @@
 struct args {
     char loc_port[ARG_BUF_SIZE];
     char ext_port[ARG_BUF_SIZE];
+    char remote_port[ARG_BUF_SIZE];
     char crone_port[ARG_BUF_SIZE];
     char framebuffer[ARG_BUF_SIZE];
 };
@@ -16,19 +17,21 @@ static struct args a = {
     .loc_port = "8888",
     .ext_port = "57120",
     .crone_port = "9999",
+    .remote_port = "10111",
     .framebuffer = "/dev/fb0",
 };
 
 int args_parse(int argc, char **argv) {
     int opt;
-    while ((opt = getopt(argc, argv, "e:l:c:f:h")) != -1) {
+    while ((opt = getopt(argc, argv, "o:e:l:c:f:h")) != -1) {
         switch (opt) {
         case '?':
         case 'h':
         default:
-            fprintf(stdout, "Start matron with optional overrides:");
+            fprintf(stdout, "Start matron with optional overrides:\n");
             fprintf(stdout, "-l   override OSC local port [default %s]\n", a.loc_port);
             fprintf(stdout, "-e   override OSC ext port [default %s]\n", a.ext_port);
+            fprintf(stdout, "-o   override OSC remote port [default %s]\n", a.remote_port);
             fprintf(stdout, "-c   override crone port [default %s]\n", a.crone_port);
             fprintf(stdout, "-f   override framebuffer file [default '%s']\n", a.framebuffer);
 
@@ -39,6 +42,9 @@ int args_parse(int argc, char **argv) {
             break;
         case 'e':
             strncpy(a.ext_port, optarg, ARG_BUF_SIZE - 1);
+            break;
+        case 'o':
+            strncpy(a.remote_port, optarg, ARG_BUF_SIZE - 1);
             break;
         case 'c':
             strncpy(a.crone_port, optarg, ARG_BUF_SIZE - 1);
@@ -57,6 +63,10 @@ const char *args_local_port(void) {
 
 const char *args_ext_port(void) {
     return a.ext_port;
+}
+
+const char *args_remote_port(void) {
+    return a.remote_port;
 }
 
 const char *args_crone_port(void) {
--- matron/src/args.h
+++ matron/src/args.h
@@ -4,6 +4,7 @@ extern int args_parse(int argc, char **argv);
 
 extern const char *args_local_port(void);
 extern const char *args_ext_port(void);
+extern const char *args_remote_port(void);
 extern const char *args_crone_port(void);
 extern const char *args_monome_path(void);
 extern const char *args_framebuffer(void);
