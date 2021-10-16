1b8c88f10b240c59c560b542dd27c3d5c53b44f2
brian crabtree <tehn@monome.org>
2021-10-16 00:31:23 -0400

    watcher pw reset msg (#1464)
    * check result of system cmds
    
    * shorten
    
    Co-authored-by: brian crabtree <tehn@nnnnnnnn.co>

--- watcher/src/main.c
+++ watcher/src/main.c
@@ -25,8 +25,9 @@ int main(void) {
   printf("watcher\n");
 
   if(access("/boot/norns.txt", F_OK)==0) {
-    system("echo 'we:sleep' | sudo chpasswd");
-    system("sudo rm /boot/norns.txt");
+    int r = system("echo 'we:sleep' | sudo chpasswd");
+    r |= system("sudo rm /boot/norns.txt");
+    fprintf(stderr, "password reset: %d\n", r);
   }
 
   int fd;
