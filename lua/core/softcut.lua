5a0124eddd39c68ace3c619e91ec6793f33fdb03
brian crabtree <tehn@monome.org>
2022-01-29 10:24:06 -0500

    Revert "stop after next loop+fade  (#1494)" (#1506)
    This reverts commit 5e9b780ec99eaa03c3d8a23a3a03cb7566ea6fe0.

--- lua/core/softcut.lua
+++ lua/core/softcut.lua
@@ -108,10 +108,6 @@ SC.pre_level = function(voice,amp) _norns.cut_param("pre_level",voice,amp) end
 -- @tparam int voice : voice number (1-?)
 -- @tparam int state : off/on (0,1)
 SC.rec = function(voice,state) _norns.cut_param("rec_flag",voice,state) end
--- set to record one loop.
--- @tparam int voice : voice number (1-?)
--- @tparam float pos : position in seconds to initiate loop (optional). if omitted, then next cut will start
-SC.rec_once = function(src, pos) _norns.cut_param("rec_once",src,pos or -1) end
 --- set record head offset
 SC.rec_offset = function(voice,value) _norns.cut_param("rec_offset",voice,value) end
 --- set play position
