cfa8fefdb5458e358aaec8fe41b1ebb969d99326
brian crabtree <tehn@monome.org>
2022-03-21 22:46:52 +0000

    two boot sessions

--- lua/core/menu/log.lua
+++ lua/core/menu/log.lua
@@ -1,40 +1,27 @@
 local m = {}
 
-local s = 500
-local go = false
-
 m.init = function()
   go = false
 end
 
 m.key = function(n,z)
-  if n==3 and z==1 then
+  if (n==2 or n==3) and z==1 then
     print("export logs!")
-    os.execute("journalctl -n "..s.." > ~/dust/data/system.log")
-    _menu.set_page("SYSTEM")
-  elseif n==2 and z==1 then
+    os.execute("echo '//////// current boot\n\n' > ~/dust/data/system.log")
+    os.execute("journalctl --output=with-unit --boot=-0 >> ~/dust/data/system.log")
+    os.execute("echo '\n\n//////// previous boot\n\n' >> ~/dust/data/system.log")
+    os.execute("journalctl --output=with-unit --boot=-1 >> ~/dust/data/system.log")
     _menu.set_page("SYSTEM")
   end
 end
 
-m.enc = function(n,d)
-  if (n==3) then
-    s = util.clamp(s + d*10,100,10000)
-    _menu.redraw()
-  end
-end
+m.enc = norns.none
 
 m.redraw = function()
   screen.clear()
   screen.move(64,40)
   screen.level(15)
-  if done then
-    screen.text_center("logs exported!")
-  else
-    screen.text_center("logs size:")
-    screen.move(64,48)
-    screen.text_center(s)
-  end
+  screen.text_center("logs exported!")
   screen.update()
 end
 
