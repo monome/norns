3dfc7a5074b893b0b1f21c30ce1b9a5e51f9d4ca
Greg Wuller <greg@afofo.com>
2022-04-03 23:31:46 -0700

    weaver: wrap jack functions introspect and alter port connections

--- matron/src/jack_client.h
+++ matron/src/jack_client.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdint.h>
+#include <stdbool.h>
 
 // exposed for the clock module?
 extern double jack_sample_rate;
@@ -10,7 +11,7 @@ extern int jack_client_init();
 
 extern void jack_client_deinit();
 
-// return the running estimate of audio CPU load reported by jack 
+// return the running estimate of audio CPU load reported by jack
 // returns ratio in [0,1]
 extern float jack_client_get_cpu_load();
 
@@ -19,3 +20,10 @@ extern uint32_t jack_client_get_xrun_count();
 
 // get JACks current system time estimate in seconds (computed from sample frames)
 extern double jack_client_get_current_time();
+
+extern const char** jack_client_get_input_ports();
+extern const char** jack_client_get_output_ports();
+extern const char** jack_client_get_port_connections(const char *port_name);
+extern void jack_client_free_port_list(const char **list);
+extern bool jack_client_connect(const char *source_name, const char *destination_name);
+extern bool jack_client_disconnect(const char *source_name, const char *destination_name);
