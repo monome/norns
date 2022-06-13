b797f04ccac2f95bbee8db9498c866ba09fa6d55
dan derks <derks.dan@gmail.com>
2022-06-13 16:25:26 -0400

    Revert "Merge branch 'main' into password-helper"
    This reverts commit 66fe7983aad172134a1409259d612a6f35976bb2, reversing
    changes made to 651d7c08ce8fe96f1a300c9756318a1004ceab15.

--- matron/src/clocks/clock_link.c
+++ matron/src/clocks/clock_link.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <unistd.h>
 
-#include <abl_link.h>
+#include <ableton_link.h>
 
 #include "clock.h"
 
@@ -28,59 +28,61 @@ static clock_reference_t clock_link_reference;
 static void *clock_link_run(void *p) {
     (void)p;
 
-    abl_link link;
-    abl_link_session_state state;
+    AbletonLink *link;
+    AbletonLinkClock *clock;
+    AbletonLinkSessionState *state;
 
-    link = abl_link_create(120);
-    state = abl_link_create_session_state();
+    link = ableton_link_new(120);
+    clock = ableton_link_clock(link);
 
     while (true) {
         if (pthread_mutex_trylock(&clock_link_shared_data.lock) == 0) {
-            abl_link_capture_app_session_state(link, state);
+            state = ableton_link_capture_app_session_state(link);
 
-            uint64_t micros = abl_link_clock_micros(link);
-            double link_tempo = abl_link_tempo(state);
-            bool link_playing = abl_link_is_playing(state);
+            uint64_t micros = ableton_link_clock_micros(clock);
+            double link_tempo = ableton_link_session_state_tempo(state);
+            bool link_playing = ableton_link_session_state_is_playing(state);
 
 
             if (clock_link_shared_data.transport_start) {
-                abl_link_set_is_playing(state, true, 0);
-                abl_link_commit_app_session_state(link, state);
+                ableton_link_session_state_set_is_playing(state, true, 0);
+                ableton_link_commit_app_session_state(link, state);
                 clock_link_shared_data.transport_start = false;
             }
 
             if (clock_link_shared_data.transport_stop) {
-                abl_link_set_is_playing(state, false, 0);
-                abl_link_commit_app_session_state(link, state);
+                ableton_link_session_state_set_is_playing(state, false, 0);
+                ableton_link_commit_app_session_state(link, state);
                 clock_link_shared_data.transport_stop = false;
             }
 
             if (clock_link_shared_data.start_stop_sync) {
                 if (!clock_link_shared_data.playing && link_playing) {
-                    abl_link_request_beat_at_start_playing_time(state, 0, clock_link_shared_data.quantum);
+                    ableton_link_session_state_request_beat_at_start_playing_time(state, 0, clock_link_shared_data.quantum);
                     clock_link_shared_data.playing = true;
 
                     // this will also reschedule pending sync events to beat 0
                     clock_start_from_source(CLOCK_SOURCE_LINK);
-                    abl_link_commit_app_session_state(link, state);
+                    ableton_link_commit_app_session_state(link, state);
                 } else if (clock_link_shared_data.playing && !link_playing) {
                     clock_link_shared_data.playing = false;
                     clock_stop_from_source(CLOCK_SOURCE_LINK);
                 }
             }
 
-            double link_beat = abl_link_beat_at_time(state, micros, clock_link_shared_data.quantum);
+            double link_beat = ableton_link_session_state_beat_at_time(state, micros, clock_link_shared_data.quantum);
             clock_update_source_reference(&clock_link_reference, link_beat, 60.0f / link_tempo);
 
             if (clock_link_shared_data.requested_tempo > 0) {
-                abl_link_set_tempo(state, clock_link_shared_data.requested_tempo, micros);
-                abl_link_commit_app_session_state(link, state);
+                ableton_link_session_state_set_tempo(state, clock_link_shared_data.requested_tempo, micros);
+                ableton_link_commit_app_session_state(link, state);
                 clock_link_shared_data.requested_tempo = 0;
             }
 
-            abl_link_enable(link, clock_link_shared_data.enabled);
-            abl_link_enable_start_stop_sync(link, clock_link_shared_data.start_stop_sync);
+            ableton_link_enable(link, clock_link_shared_data.enabled);
+            ableton_link_enable_start_stop_sync(link, clock_link_shared_data.start_stop_sync);
 
+            ableton_link_session_state_destroy(state);
             pthread_mutex_unlock(&clock_link_shared_data.lock);
         }
 
