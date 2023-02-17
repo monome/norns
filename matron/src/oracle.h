ae782f83d0e39ad0821f55ece91be736d6aeef77
Rylee Alanza Lyman <46907231+ryleelyman@users.noreply.github.com>
2023-02-17 18:08:34 +0100

    fix: catch typo

--- matron/src/oracle.h
+++ matron/src/oracle.h
@@ -69,7 +69,7 @@ extern void o_unlock_descriptors();
 //-----------------------
 //--- IPC functions
 
-// load a named audio erocessing engine
+// load a named audio processing engine
 extern void o_load_engine(const char *name);
 // free engine
 extern void o_free_engine(void);
