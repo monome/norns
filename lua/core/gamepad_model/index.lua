c693f6b45bae59111d764cd3c6667ce1a6a624a9
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-04-09 00:02:13 +0200

    allow user to add gamepad models without tempering w/ core

--- lua/core/gamepad_model/index.lua
+++ lua/core/gamepad_model/index.lua
@@ -1,7 +1,23 @@
 
 local models = {}
 
+-- bundled w/ core
 models['03000000830500006020000010010000'] = require 'gamepad_model/ibufalo_classic_usb'
 models['030000005e0400008e02000014010000'] = require 'gamepad_model/xbox_360'
+
+-- user-local
+local user_conf_dir = _path.data .. "gamepads/"
+if util.file_exists(user_conf_dir) then
+  local user_confs = util.scandir(user_conf_dir)
+  for _, conf in pairs(user_confs) do
+    if conf:sub(-#'.lua') == '.lua' then
+      local conf_sans_ext = conf:gsub("%.lua", "")
+      print("loading user gamepad conf: "..user_conf_dir..conf)
+      local module_path = user_conf_dir .. conf_sans_ext
+      local user_conf = require(module_path)
+      models[user_conf.guid] = user_conf
+    end
+  end
+end
 
 return models
