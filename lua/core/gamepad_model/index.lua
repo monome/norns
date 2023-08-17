edd58281310fc88d9222e920b818e9558363bc6a
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-08-17 23:56:05 +0200

    new gamepad model: Retrolink B00GWKL3Y4

--- lua/core/gamepad_model/index.lua
+++ lua/core/gamepad_model/index.lua
@@ -4,6 +4,7 @@ local models = {}
 -- bundled w/ core
 models['03000000830500006020000010010000'] = require 'gamepad_model/ibufalo_classic_usb'
 models['030000005e0400008e02000014010000'] = require 'gamepad_model/xbox_360'
+models['03000000790000001100000010010000'] = require 'gamepad_model/retro_controller'
 
 -- user-local
 local user_conf_dir = _path.data .. "gamepads/"
