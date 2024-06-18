monome/norns

commit e11b68e1b931958f61e32c6220dd136a0fefe2ff

binary triggers: allow default of '1' (#1785)

lua/core/paramset.lua
@@ -544,7 +544,11 @@ function ParamSet:read(filename, silent)
 
           if index and self.params[index] and not param_already_set[index] then
             if tonumber(value) ~= nil then
-              self.params[index]:set(tonumber(value), silent)
+              if self.params[index].t == self.tBINARY then
+                if self.params[index].behavior ~= "trigger" then
+                  self.params[index]:set(tonumber(value), silent)
+                end
+              end
             elseif value == "-inf" then
               self.params[index]:set(-math.huge, silent)
             elseif value == "inf" then
@@ -590,7 +594,7 @@ end
 --- bang all params.
 function ParamSet:bang()
   for _,v in pairs(self.params) do
-    if v.t ~= self.tTRIGGER and not (v.t == self.tBINARY and v.behavior == 'trigger') then
+    if v.t ~= self.tTRIGGER and not (v.t == self.tBINARY and v.behavior == 'trigger' and v.value == 0) then
       v:bang()
     end
   end

lua/core/params/binary.lua
@@ -11,9 +11,9 @@ function Binary.new(id, name, behavior, default, allow_pmap)
   o.t = tBINARY
   o.id = id
   o.name = name
-  o.default = default or 0
+  o.default = default or (o.behavior ~= 'trigger' and 0 or 1)
+  o.value = o.default
   o.behavior = behavior or 'trigger'
-  o.value = o.behavior ~= 'trigger' and o.default or 1
   o.action = function() end
   if allow_pmap == nil then o.allow_pmap = true else o.allow_pmap = allow_pmap end
   return o
@@ -56,6 +56,9 @@ end
 
 function Binary:bang()
   self.action(self.behavior == 'trigger' and 1 or self.value)
+  if self.behavior == 'trigger' then
+    self.value = 0
+  end
 end
 
 function Binary:string()

commit e47690251c2f86abccf6da393c78c94b106c4450

fix for paramset:read() not setting values
Fixes issue introduced in #1785 which prevents paramset:read() from setting non-binary param values on .pset load.

lua/core/paramset.lua
@@ -548,6 +548,8 @@ function ParamSet:read(filename, silent)
                 if self.params[index].behavior ~= "trigger" then
                   self.params[index]:set(tonumber(value), silent)
                 end
+              else
+                self.params[index]:set(tonumber(value), silent)
               end
             elseif value == "-inf" then
               self.params[index]:set(-math.huge, silent)