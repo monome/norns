de98bd2debfe1e71edddd75f910e770ce3aa37f6
Steven Noreyko <1909661+okyeron@users.noreply.github.com>
2025-04-05 12:44:28 -0500

    Add more Platforms

--- matron/src/hardware/platform.c
+++ matron/src/hardware/platform.c
@@ -2,16 +2,36 @@
 #include <stdlib.h>
 
 #include "platform.h"
+#include <string.h>
+#include <stdio.h>
 
 platform_t p;
 
 void init_platform() {
   if(access("/sys/firmware/devicetree/base/model", F_OK ) != -1) {
-    if(system("cat /sys/firmware/devicetree/base/model | grep 'Compute'")) {
-      p = PLATFORM_PI3;
-    } else {
-      p = PLATFORM_CM3;
-    }
+
+	FILE* fptr = fopen("/sys/firmware/devicetree/base/model","r");
+	char modelString[100];
+	fgets(modelString, 100, fptr);
+	fclose(fptr);
+
+   if (strstr(modelString, "Compute Module 3")){
+     p = PLATFORM_CM3;
+   } else if (strstr(modelString, "Compute Module 4")){
+     p = PLATFORM_CM4;   
+   } else if (strstr(modelString, "Compute Module 4S")){
+     p = PLATFORM_CM4S;   
+   } else if (strstr(modelString, "Raspberry Pi 3")){
+     p = PLATFORM_PI3;
+   } else if (strstr(modelString, "Raspberry Pi 4")){
+     p = PLATFORM_PI4;
+   }
+
+//     if(system("cat /sys/firmware/devicetree/base/model | grep 'Compute Module 3'")) {
+//       p = PLATFORM_PI3;
+//     } else {
+//       p = PLATFORM_CM3;
+//     }
   } else {
     p = PLATFORM_OTHER;
   }
