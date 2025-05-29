6737d1b4dbd509a1a31b1a79308e7b82e262b1cb
Greg Wuller <greg@afofo.com>
2025-05-28 17:41:09 -0700

    fix cm4s platform detection and screen (#1835)
    * fix cm4s platform detection and screen
    
    * ensure platform is initialized

--- matron/src/hardware/platform.c
+++ matron/src/hardware/platform.c
@@ -5,32 +5,53 @@
 #include <string.h>
 #include <stdio.h>
 
-platform_t p;
+static platform_t p = PLATFORM_UNKNOWN;
 
 void init_platform() {
   if(access("/sys/firmware/devicetree/base/model", F_OK ) != -1) {
 
-	FILE* fptr = fopen("/sys/firmware/devicetree/base/model","r");
-	char modelString[100];
-	fgets(modelString, 100, fptr);
-	fclose(fptr);
+    FILE* fptr = fopen("/sys/firmware/devicetree/base/model","r");
+    char modelString[100];
+    fgets(modelString, 100, fptr);
+    fclose(fptr);
 
-   if (strstr(modelString, "Compute Module 3")){
-     p = PLATFORM_CM3;
-   } else if (strstr(modelString, "Compute Module 4")){
-     p = PLATFORM_CM4;   
-   } else if (strstr(modelString, "Compute Module 4S")){
-     p = PLATFORM_CM4S;   
-   } else if (strstr(modelString, "Raspberry Pi 3")){
-     p = PLATFORM_PI3;
-   } else if (strstr(modelString, "Raspberry Pi 4")){
-     p = PLATFORM_PI4;
-   }
-  } else {
-    p = PLATFORM_OTHER;
+    if (strstr(modelString, "Compute Module 3")){
+      p = PLATFORM_CM3;
+    } else if (strstr(modelString, "Compute Module 4S")){
+      p = PLATFORM_CM4S;
+    } else if (strstr(modelString, "Compute Module 4")){
+      p = PLATFORM_CM4;
+    } else if (strstr(modelString, "Raspberry Pi 3")){
+      p = PLATFORM_PI3;
+    } else if (strstr(modelString, "Raspberry Pi 4")){
+      p = PLATFORM_PI4;
+    } else {
+      p = PLATFORM_OTHER;
+    }
   }
 }
 
 platform_t platform() {
   return p;
+}
+
+const char* platform_name() {
+  switch (platform()) {
+  case PLATFORM_CM3:
+    return "cm3";
+  case PLATFORM_CM4:
+    return "cm4";
+  case PLATFORM_CM4S:
+    return "cm4s";
+  case PLATFORM_PI3:
+    return "pi3";
+  case PLATFORM_PI4:
+    return "pi4";
+  case PLATFORM_OTHER:
+    return "other";
+  default:
+    break;
+  }
+
+  return "unknown";
 }
