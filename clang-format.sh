12c8afb7531868714dc157cb7043fb33da18ae8b
Colin McArdell <colin@colinmcardell.com>
2025-08-08 17:13:20 -0400

    update clang-format script to include additional source directories

--- clang-format.sh
+++ clang-format.sh
@@ -2,3 +2,6 @@
 # run this from top-level norns directory before committing...
 find ws-wrapper/src -regex .*[\.][ch] | xargs clang-format -i
 find matron/src -regex .*[\.][ch] | xargs clang-format -i
+find maiden-repl/src -regex .*[\.][ch] | xargs clang-format -i
+find crone/src -type f \( -name '*.c' -o -name '*.h' -o -name '*.cpp' \) | xargs clang-format -i
+find watcher/src -regex .*[\.][ch] | xargs clang-format -i
