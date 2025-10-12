98baa7465374b6161555ed8bc00b10f61e095511
Colin McArdell <colin@colinmcardell.com>
2025-10-12 17:27:19 -0400

    add tests directory to clang-format
    formats test files (.c, .h, .cpp, .hpp) alongside system sources.

--- clang-format.sh
+++ clang-format.sh
@@ -5,3 +5,4 @@ find matron/src -regex .*[\.][ch] | xargs clang-format -i
 find maiden-repl/src -regex .*[\.][ch] | xargs clang-format -i
 find crone/src -type f \( -name '*.c' -o -name '*.h' -o -name '*.cpp' \) | xargs clang-format -i
 find watcher/src -regex .*[\.][ch] | xargs clang-format -i
+find tests -type f \( -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.hpp' \) | xargs clang-format -i
