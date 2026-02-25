eaa0245f67d6694bcebe9d9f342fda5459e39a7f
Colin McArdell <colin@colinmcardell.com>
2026-02-25 10:56:53 -0500

    refactor: replace custom queue in sidecar with BlockingReaderWriterQueue

--- norns/sidecar.cpp
+++ norns/sidecar.cpp
@@ -1,6 +1,5 @@
 #include <assert.h>
 #include <pthread.h>
-#include <search.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +10,7 @@
 #include <nng/protocol/reqrep0/req.h>
 #include <nng/transport/ipc/ipc.h>
 
+#include "readerwriterqueue.h"
 #include "sidecar.h"
 
 //---------------------------------
@@ -19,7 +19,7 @@
 const char *url = "ipc:///tmp/norns-sidecar.ipc";
 
 static void sidecar_nng_error(const char *func, int rv) {
-  fprintf(stderr, "%s: %s\n", func, nng_strerror(rv));
+    fprintf(stderr, "%s: %s\n", func, nng_strerror(rv));
 }
 
 //---------------------------------
@@ -47,208 +47,146 @@ union request {
     struct request_run_cmd run_cmd;
 };
 
-struct req_node {
-    struct req_node *next;
-    struct req_node *prreq;
-    union request *req;
-};
-
-struct req_q {
-    struct req_node *head;
-    struct req_node *tail;
-    ssize_t size;
-    pthread_cond_t nonempty;
-    pthread_mutex_t lock;
-};
-
-static struct req_q requests;
-
-static void requests_push(union request *req) {
-    struct req_node *node = (struct req_node*)calloc(1, sizeof(struct req_node));
-    node->req = req;
-    if (requests.size == 0) {
-        insque(node, NULL);
-        requests.head = node;
-    } else {
-        insque(node, requests.tail);
-    }
-    requests.tail = node;
-    requests.size += 1;
-}
-
-static union request *requests_pop() {
-    struct req_node *node = requests.head;
-    if (node == NULL) {
-        return NULL;
-    }
-    union request *req = node->req;
-    requests.head = node->next;
-    if (node == requests.tail) {
-        assert(requests.size == 1);
-        requests.tail = NULL;
-    }
-    remque(node);
-    free(node);
-    requests.size -= 1;
-    return req;
-}
-
-static void requests_init(void) {
-    requests.size = 0;
-    requests.head = NULL;
-    requests.tail = NULL;
-    pthread_cond_init(&requests.nonempty, NULL);
-}
+static moodycamel::BlockingReaderWriterQueue<union request *> requests(32);
 
 static union request *request_new(request_type type) {
-    // FIXME: better not to allocate here, use object pool
-    union request *req = (request*)calloc(1, sizeof(union request));
+    union request *req = (request *)calloc(1, sizeof(union request));
     req->type = type;
     return req;
 }
 
 static void request_post(union request *req) {
     assert(req != NULL);
-    pthread_mutex_lock(&requests.lock);
-    if (requests.size == 0) {
-        // signal handler thread to wake up...
-        pthread_cond_signal(&requests.nonempty);
-    }
-    requests_push(req);
-    // ...handler actually wakes up once we release the lock
-    pthread_mutex_unlock(&requests.lock);
-
+    requests.enqueue(req);
 }
-
 
 //---------------------------------
 //--- server
 
-const size_t MAX_CAPTURE_BYTES = 1024 * 1000;  // 10MB maximum
+const size_t MAX_CAPTURE_BYTES = 1024 * 1000; // 10MB maximum
 const size_t READ_BUFFER_BYTES = 8192;
 static char read_buffer[READ_BUFFER_BYTES];
 
 static nng_msg *sidecar_server_run_cmd(const char *cmd) {
-  FILE *f = popen((char *)cmd, "r");
-  if (f == NULL) {
-    fprintf(stderr, "popen() failed\n");
-    return NULL;
-  }
+    FILE *f = popen((char *)cmd, "r");
+    if (f == NULL) {
+        fprintf(stderr, "popen() failed\n");
+        return NULL;
+    }
 
-  int status;
-  nng_msg *msg = NULL;
-  size_t capture_bytes = 0;
-  size_t read_bytes = 0;
+    int status;
+    nng_msg *msg = NULL;
+    size_t capture_bytes = 0;
+    size_t read_bytes = 0;
 
-  // NOTE: initial allocation is 0 because nng_msg_append* place values at the
-  // end of the msg, allocating more space as needed. allocating space up front
-  // does not behave like a capacity reservation.
-  status = nng_msg_alloc(&msg, 0);
-  if (status != 0) {
-    fprintf(stderr, "nng_msg_alloc() failed\n");
-    return NULL;
-  }
-
-  while ((read_bytes = fread(read_buffer, 1, READ_BUFFER_BYTES, f)) > 0) {
-    status = nng_msg_append(msg, read_buffer, read_bytes);
+    // NOTE: initial allocation is 0 because nng_msg_append* place values at the
+    // end of the msg, allocating more space as needed. allocating space up front
+    // does not behave like a capacity reservation.
+    status = nng_msg_alloc(&msg, 0);
     if (status != 0) {
-      pclose(f);
-      nng_msg_free(msg);
-      fprintf(stderr, "nng_msg_append() failed\n");
-      return NULL;
+        fprintf(stderr, "nng_msg_alloc() failed\n");
+        return NULL;
     }
 
-    capture_bytes += read_bytes;
+    while ((read_bytes = fread(read_buffer, 1, READ_BUFFER_BYTES, f)) > 0) {
+        status = nng_msg_append(msg, read_buffer, read_bytes);
+        if (status != 0) {
+            pclose(f);
+            nng_msg_free(msg);
+            fprintf(stderr, "nng_msg_append() failed\n");
+            return NULL;
+        }
 
-    if (capture_bytes > MAX_CAPTURE_BYTES) {
-      pclose(f);
-      nng_msg_free(msg);
-      fprintf(stderr, "sidecar: command output too large, %d bytes maxiumum\n", MAX_CAPTURE_BYTES);
-      return NULL;
+        capture_bytes += read_bytes;
+
+        if (capture_bytes > MAX_CAPTURE_BYTES) {
+            pclose(f);
+            nng_msg_free(msg);
+            fprintf(stderr, "sidecar: command output too large, %d bytes maxiumum\n", MAX_CAPTURE_BYTES);
+            return NULL;
+        }
     }
-  }
 
-  // append a null byte to the end of the buffer
-  nng_msg_append_u16(msg, 0);
+    // append a null byte to the end of the buffer
+    nng_msg_append_u16(msg, 0);
 
-  pclose(f);
-  return msg;
+    pclose(f);
+    return msg;
 }
 
 int sidecar_server_main(int sync_fd) {
-  nng_socket sock;
-  nng_listener listener;
-  int rv;
+    nng_socket sock;
+    nng_listener listener;
+    int rv;
 
-  nng_ipc_register();
+    nng_ipc_register();
 
-  if ((rv = nng_rep0_open(&sock)) != 0) {
-    sidecar_nng_error("open socket failed", rv);
+    if ((rv = nng_rep0_open(&sock)) != 0) {
+        sidecar_nng_error("open socket failed", rv);
+        close(sync_fd);
+        return -1;
+    }
+
+    if ((rv = nng_listen(sock, url, &listener, 0)) != 0) {
+        nng_close(sock);
+        sidecar_nng_error("binding socket failed", rv);
+        close(sync_fd);
+        return -1;
+    }
+
+    // signal parent that IPC is bound and ready
+    char b = 1;
+    write(sync_fd, &b, 1);
     close(sync_fd);
-    return -1;
-  }
 
-  if ((rv = nng_listen(sock, url, &listener, 0)) != 0) {
+    for (;;) {
+        char *cmd = NULL;
+        size_t cmd_sz = 0;
+
+        if ((rv = nng_recv(sock, &cmd, &cmd_sz, NNG_FLAG_ALLOC)) != 0) {
+            sidecar_nng_error("recv error", rv);
+            continue;
+        }
+
+        // handle quit command
+        if (cmd != NULL && cmd_sz >= 8 && strncmp((char *)cmd, "__quit__", 8) == 0) {
+            nng_msg *response;
+            nng_msg_alloc(&response, 0);
+            nng_msg_append_u16(response, 0);
+            nng_sendmsg(sock, response, 0);
+            nng_free(cmd, cmd_sz);
+            break;
+        }
+
+        // handle run command
+        nng_msg *response = sidecar_server_run_cmd(cmd);
+        if (response == NULL) {
+            // allocate an empty message if the command failed, so the client doesn't block forever
+            nng_msg_alloc(&response, 0);
+            nng_msg_append_u16(response, 0);
+        }
+
+        if ((rv = nng_sendmsg(sock, response, 0)) != 0) {
+            nng_msg_free(response);
+            sidecar_nng_error("send error", rv);
+        }
+
+        if (cmd != NULL) {
+            nng_free(cmd, cmd_sz);
+        }
+    }
+
     nng_close(sock);
-    sidecar_nng_error("binding socket failed", rv);
-    close(sync_fd);
-    return -1;
-  }
-
-  // signal parent that IPC is bound and ready
-  char b = 1;
-  write(sync_fd, &b, 1);
-  close(sync_fd);
-
-  for (;;) {
-    char *cmd = NULL;
-    size_t cmd_sz = 0;
-
-    if ((rv = nng_recv(sock, &cmd, &cmd_sz, NNG_FLAG_ALLOC)) != 0) {
-      sidecar_nng_error("recv error", rv);
-      continue;
-    }
-
-    // handle quit command
-    if (cmd != NULL && cmd_sz >= 8 && strncmp((char *)cmd, "__quit__", 8) == 0) {
-      nng_msg *response;
-      nng_msg_alloc(&response, 0);
-      nng_msg_append_u16(response, 0);
-      nng_sendmsg(sock, response, 0);
-      nng_free(cmd, cmd_sz);
-      break;
-    }
-
-    // handle run command
-    nng_msg *response = sidecar_server_run_cmd(cmd);
-    if (response == NULL) {
-      // allocate an empty message if the command failed, so the client doesn't block forever
-      nng_msg_alloc(&response, 0);
-      nng_msg_append_u16(response, 0);
-    }
-
-    if ((rv = nng_sendmsg(sock, response, 0)) != 0) {
-      nng_msg_free(response);
-      sidecar_nng_error("send error", rv);
-    }
-
-    if (cmd != NULL) {
-      nng_free(cmd, cmd_sz);
-    }
-  }
-
-  nng_close(sock);
-  return 0;
+    return 0;
 }
 
 //---------------------------------
 //--- client
 
-
 struct client_state {
-  nng_socket sock;
-  nng_dialer dialer;
-  bool initialized;
+    nng_socket sock;
+    nng_dialer dialer;
+    bool initialized;
 };
 
 static pthread_t client_thread;
@@ -256,77 +194,70 @@ static struct client_state cs;
 static pthread_mutex_t run_cmd_lock;
 
 static void handle_run_cmd(const char *cmd, void *ctx, client_cmd_completion_t completion) {
-  static char empty_result[1] = { '\0' };
+    static char empty_result[1] = {'\0'};
 
-  int rv;
-  char *result = NULL;
-  size_t result_sz = 0;
-  char *recv_buf = NULL;
-  size_t recv_sz = 0;
+    int rv;
+    char *result = NULL;
+    size_t result_sz = 0;
+    char *recv_buf = NULL;
+    size_t recv_sz = 0;
 
-  // serialize interaction with the sidecar so sync and async command invocation
-  // is not interlieved
-  pthread_mutex_lock(&run_cmd_lock);
+    // serialize interaction with the sidecar so sync and async command invocation
+    // is not interlieved
+    pthread_mutex_lock(&run_cmd_lock);
 
-  if ((rv = nng_send(cs.sock, (void *)cmd, strlen(cmd) + 1, 0)) != 0) {
-    sidecar_nng_error("sidecar: send error", rv);
-    goto complete;
-  }
+    if ((rv = nng_send(cs.sock, (void *)cmd, strlen(cmd) + 1, 0)) != 0) {
+        sidecar_nng_error("sidecar: send error", rv);
+        goto complete;
+    }
 
-  // TODO: investigate using a pre-allocated receive buffer
-  if ((rv = nng_recv(cs.sock, &recv_buf, &recv_sz, NNG_FLAG_ALLOC)) != 0) {
-    sidecar_nng_error("sidecar: recv error", rv);
-    goto complete;
-  }
+    // TODO: investigate using a pre-allocated receive buffer
+    if ((rv = nng_recv(cs.sock, &recv_buf, &recv_sz, NNG_FLAG_ALLOC)) != 0) {
+        sidecar_nng_error("sidecar: recv error", rv);
+        goto complete;
+    }
 
-  if (recv_sz > 0) {
-    result = recv_buf;
-    result_sz = recv_sz;
-  } else {
-    result = empty_result;
-    result_sz = 1;
-  }
+    if (recv_sz > 0) {
+        result = recv_buf;
+        result_sz = recv_sz;
+    } else {
+        result = empty_result;
+        result_sz = 1;
+    }
 
 complete:
-  pthread_mutex_unlock(&run_cmd_lock);
-  completion(cmd, ctx, result, result_sz);
-  nng_free(recv_buf, recv_sz);
+    pthread_mutex_unlock(&run_cmd_lock);
+    completion(cmd, ctx, result, result_sz);
+    nng_free(recv_buf, recv_sz);
 }
 
 static void handle_request(union request *req) {
-  switch (req->type) {
-  case REQUEST_RUN_CMD:
-    handle_run_cmd(req->run_cmd.cmd, req->run_cmd.ctx, req->run_cmd.completion_handler);
-    free(req->run_cmd.cmd);
-    break;
-  default:
-    fprintf(stderr, "sidecar: unhandled event type; %d\n", req->type);
-    break;
-  }
+    switch (req->type) {
+    case REQUEST_RUN_CMD:
+        handle_run_cmd(req->run_cmd.cmd, req->run_cmd.ctx, req->run_cmd.completion_handler);
+        free(req->run_cmd.cmd);
+        break;
+    default:
+        fprintf(stderr, "sidecar: unhandled event type; %d\n", req->type);
+        break;
+    }
 }
 
 static void *sidecar_client_loop(void *) {
-  union request *req = NULL;
+    union request *req = nullptr;
 
-  for (;;) {
-    pthread_mutex_lock(&requests.lock);
-    while (requests.size == 0) {
-      pthread_cond_wait(&requests.nonempty, &requests.lock);
-    }
-    req = requests_pop();
-    pthread_mutex_unlock(&requests.lock);
-    if (req != NULL) {
-      if (req->type == REQUEST_QUIT) {
+    for (;;) {
+        requests.wait_dequeue(req);
+        if (req->type == REQUEST_QUIT) {
+            free(req);
+            break;
+        }
+        handle_request(req);
         free(req);
-        break;
-      }
-      handle_request(req);
-      free(req);
     }
-  }
 
-  nng_close(cs.sock);
-  return NULL;
+    nng_close(cs.sock);
+    return NULL;
 }
 
 //
@@ -334,33 +265,31 @@ static void *sidecar_client_loop(void *) {
 //
 
 int sidecar_client_init() {
-  int rv;
+    int rv;
 
-  nng_ipc_register();
+    nng_ipc_register();
 
-  cs.initialized = false;
+    cs.initialized = false;
 
-  if ((rv = nng_req0_open(&cs.sock)) != 0) {
-    sidecar_nng_error("sidecar: open socket failed", rv);
-    return -1;
-  }
+    if ((rv = nng_req0_open(&cs.sock)) != 0) {
+        sidecar_nng_error("sidecar: open socket failed", rv);
+        return -1;
+    }
 
-  if ((rv = nng_dial(cs.sock, url, &cs.dialer, 0)) != 0) {
-    sidecar_nng_error("sidecar: establishing connection failed", rv);
-    return -1;
-  }
+    if ((rv = nng_dial(cs.sock, url, &cs.dialer, 0)) != 0) {
+        sidecar_nng_error("sidecar: establishing connection failed", rv);
+        return -1;
+    }
 
-  cs.initialized = true;
+    cs.initialized = true;
 
-  requests_init();
+    if (pthread_create(&client_thread, NULL, sidecar_client_loop, NULL)) {
+        fprintf(stderr, "sidecar: unable to create client thread\n");
+        return -1;
+    }
+    pthread_setname_np(client_thread, "client_loop");
 
-  if (pthread_create(&client_thread, NULL, sidecar_client_loop, NULL)) {
-    fprintf(stderr, "sidecar: unable to create client thread\n");
-    return -1;
-  }
-  pthread_setname_np(client_thread, "client_loop");
-
-  return 0;
+    return 0;
 }
 
 static void _null_completion(const char *cmd, void *ctx, const char *buf, size_t size) {
@@ -371,54 +300,54 @@ static void _null_completion(const char *cmd, void *ctx, const char *buf, size_t
 }
 
 void sidecar_client_cleanup() {
-  if (!cs.initialized) {
-    return;
-  }
+    if (!cs.initialized) {
+        return;
+    }
 
-  // Tell the sidecar server to exit
-  sidecar_client_cmd("__quit__", NULL, _null_completion);
+    // Tell the sidecar server to exit
+    sidecar_client_cmd("__quit__", NULL, _null_completion);
 
-  // Tell the client thread to exit
-  union request *req = request_new(REQUEST_QUIT);
-  request_post(req);
+    // Tell the client thread to exit
+    union request *req = request_new(REQUEST_QUIT);
+    request_post(req);
 
-  // Wait for the background thread to finish
-  pthread_join(client_thread, NULL);
+    // Wait for the background thread to finish
+    pthread_join(client_thread, NULL);
 
-  cs.initialized = false;
+    cs.initialized = false;
 }
 
 bool sidecar_client_cmd_async(const char *cmd, void *ctx, client_cmd_completion_t cb) {
-  union request *req = request_new(REQUEST_RUN_CMD);
-  req->run_cmd.cmd = strdup(cmd);
-  req->run_cmd.ctx = ctx;
-  req->run_cmd.completion_handler = cb;
-  request_post(req);
-  return true;
+    union request *req = request_new(REQUEST_RUN_CMD);
+    req->run_cmd.cmd = strdup(cmd);
+    req->run_cmd.ctx = ctx;
+    req->run_cmd.completion_handler = cb;
+    request_post(req);
+    return true;
 }
 
 bool sidecar_client_cmd(const char *cmd, void *ctx, client_cmd_completion_t cb) {
-  handle_run_cmd(cmd, ctx, cb);
-  return true;
+    handle_run_cmd(cmd, ctx, cb);
+    return true;
 }
 
 typedef struct {
-  char **result;
-  size_t *result_size;
+    char **result;
+    size_t *result_size;
 } buffer_fill_ctx;
 
 static void _buffer_fill_completion(const char *cmd, void *ctx, const char *buf, size_t size) {
-  buffer_fill_ctx *output = (buffer_fill_ctx *)ctx;
-  *(output->result_size) = size;
-  if (size != 0) {
-    *(output->result) = (char *)malloc(size);
-    memcpy(*(output->result), buf, size);
-  } else {
-    *(output->result) = NULL;
-  }
+    buffer_fill_ctx *output = (buffer_fill_ctx *)ctx;
+    *(output->result_size) = size;
+    if (size != 0) {
+        *(output->result) = (char *)malloc(size);
+        memcpy(*(output->result), buf, size);
+    } else {
+        *(output->result) = NULL;
+    }
 }
 
 void sidecar_client_cmd(const char *cmd, char **dst, size_t *dst_size) {
-  buffer_fill_ctx ctx = { dst, dst_size };
-  handle_run_cmd(cmd, &ctx, _buffer_fill_completion);
+    buffer_fill_ctx ctx = {dst, dst_size};
+    handle_run_cmd(cmd, &ctx, _buffer_fill_completion);
 }
